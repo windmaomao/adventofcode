023b4eb5d9a131f1340be45969bcbc30a80d9932
Fang <windmaomao@gmail.com>
2019-12-03 16:25:46 -0500

    add day3 part 2

--- 2019/day3.js
+++ 2019/day3.js
@@ -8,28 +8,38 @@ const lines = [
   'L1005,D527,R864,D622,R482,D647,R29,U459,R430,D942,R550,D163,L898,U890,L271,D216,L52,U731,R715,U925,L614,U19,R687,D832,L381,U192,L293,D946,L642,D2,L124,U66,R492,U281,R181,U624,R294,U767,R443,U424,R241,D225,R432,D419,L647,U290,L647,D985,L694,D777,L382,D231,R809,D467,L917,D217,R422,U490,L873,D537,R176,U856,L944,D875,L485,D49,R333,D220,L354,U789,R256,D73,R905,U146,R798,D429,R111,D585,L275,D471,R220,D619,L680,U757,R580,U497,L620,U753,R58,U574,L882,U484,R297,D899,L95,D186,R619,D622,R65,U714,L402,U950,R647,D60,L659,U101,L917,D736,L531,U398,R26,U134,R837,U294,R364,D55,R254,D999,R868,U978,R434,U661,R362,D158,L50,D576,L146,D249,L562,D433,R206,D376,L650,U285,L427,D406,L526,D597,R557,U554,L463,D157,L811,U961,R648,D184,L962,U695,R138,U661,L999,U806,L413,U54,L865,U931,L319,U235,L794,D12,L456,D918,L456,U214,L739,D772,R90,D478,R23,D658,R919,D990,L307,D534,L40,D324,L4,U805,L605,U534,R727,U452,R733,D416,L451,U598,R215,D545,L563,D222,L295,D669,R706,U11,R44,D392,L518,D437,L634,U874,L641,U240,L11,D279,L153,U601,L238,U924,L292,D406,L360,D203,R874,D506,R806,U9,R713,D891,L587,U538,L867,D637,R889,U186,R728,D672,R573,U461,R222,D703,R178,U336,L896,D924,L445,D365,L648,U3,L734,U959,R344,U314,R331,D929,L364,D937,L896,D191,R218,U256,L975,D506,R510,D392,R878,U896,L177,U4,R516,D873,R57,D530,R140,D827,L263,U848,L88,U309,L801,U670,R874,D358,L49,D259,L188,U419,R705,D498,R496,U576,R808,D959,L861,U437,L618,D112,R725,D546,R338,U879,R522,U892,R230,D367,R901,D737,L942,D689,R976,D369,R157'
 ]
 
+// const lines = [
+//   'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+//   'U62,R66,U55,R34,D71,R55,D58,R83'
+// ]
+
+// const lines = [
+//   'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+//   'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+// ]
+
 const getPosArr = line => {
   const parts = line.split(',')
-  const o = { x: 0, y: 0 }
+  const o = { x: 0, y: 0, l: 0 }
   return parts.reduce((acc, ins) => {
     const char = ins[0]
     const num = parseInt(ins.substring(1))
-    // acc.h.push({ x: char, y: num })
+    o.l = o.l + num
     switch (char) {
       case 'R':
-        acc.h.push({y: o.y, x: [o.x, o.x + num]})
+        acc.h.push({y: o.y, x: [o.x, o.x + num], d: 1, l: o.l, s: num })
         o.x = o.x + num
         break
       case 'U':
-        acc.v.push({x: o.x, y: [o.y, o.y + num]})
+        acc.v.push({ x: o.x, y: [o.y, o.y + num], d: 1, l: o.l, s: num })
         o.y = o.y + num
         break
       case 'L':
-        acc.h.push({y: o.y, x: [o.x - num, o.x]})
+        acc.h.push({ y: o.y, x: [o.x - num, o.x], d: -1, l: o.l, s: num })
         o.x = o.x - num
         break
       case 'D':
-        acc.v.push({x: o.x, y: [o.y - num, o.y]})
+        acc.v.push({ x: o.x, y: [o.y - num, o.y], d: -1, l: o.l, s: num })
         o.y = o.y - num
         break
     }
@@ -41,31 +51,39 @@ const l1 = getPosArr(lines[0])
 const l2 = getPosArr(lines[1])
 // console.log(l1,l2)
 
+const findInter = (h, v) => {
+  if (h.y <= v.y[1] && h.y >= v.y[0] && v.x <= h.x[1] && v.x >= h.x[0]) {
+    const pY = (v.d < 0) ? h.y - v.y[0] : v.y[1] - h.y
+    const pX = (h.d < 0) ? v.x - h.x[0] : h.x[1] - v.x
+    return { x: v.x, y: h.y, l: h.l - pX + v.l - pY }
+  }
+  return false
+}
+
 const getInters = (l1, l2) => {
   const inters = []
   l1.h.forEach(h => {
     l2.v.forEach(v => {
-      if (h.y <= v.y[1] && h.y >= v.y[0] && v.x <= h.x[1] && v.x >= h.x[0]) {
-        inters.push({ x: v.x, y: h.y })
-      }
+      const found = findInter(h, v)
+      if (found) inters.push(found)
     }) 
   })
   l1.v.forEach(v => {
     l2.h.forEach(h => {
-      if (h.y <= v.y[1] && h.y >= v.y[0] && v.x <= h.x[1] && v.x >= h.x[0]) {
-        inters.push({ x: v.x, y: h.y })
-      }
+      const found = findInter(h, v)
+      if (found) inters.push(found)
     })
   })
   return inters
 }
+
 
 // 266
 const ints = getInters(l1, l2)
 let len = 10000000
 const mLen = ints.reduce((acc, p) => {
   const tmp = p.x * p.x + p.y * p.y
-  if (tmp < len) { 
+  if (tmp < len && tmp > 0) { 
     console.log(p)
     len = tmp
     return Math.abs(p.x) + Math.abs(p.y)
@@ -73,4 +91,16 @@ const mLen = ints.reduce((acc, p) => {
   return acc
 }, 0)
 
-console.log(mLen)
+console.log('Day 3-1:', mLen)
+
+let len2 = 10000000
+const sLen = ints.reduce((acc, p) => {
+  if (p.l < len2 && p.l > 0) {
+    console.log(p)
+    len2 = p.l
+  }
+  return
+}, 0)
+
+console.log('Day 3-2:', len2)
+
