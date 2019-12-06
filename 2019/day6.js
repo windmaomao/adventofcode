13e232f35520ee0d55564ce8c23606d2088a6011
Fang <windmaomao@gmail.com>
2019-12-06 09:57:53 -0500

    add day6 part2

--- 2019/day6.js
+++ 2019/day6.js
@@ -42,26 +42,44 @@ console.log('Day 5-1:', nodeKeys.reduce(
   }, 0
 ))
 
-function tranv2(pk, found) {
+// COM, K9X, 1PX
+
+function tranv2(pk, target, found, lvl) {
   // console.log(pk)
   if (found) return true
-  if (pk === 'YOU') return true
+  if (pk === target) {
+    console.log('Found', lvl)
+    return true
+  }
 
   const parent = nodeMap[pk]
   if (!parent.cs.length) return false
 
   for(let i=0; i<parent.cs.length; i++) {
     const ck = parent.cs[i]
-    const f2 = tranv2(ck, false)
+    const f2 = tranv2(ck, target, false, lvl+1)
     if (f2) return true
   }
+  return false
 }
 
-roots.forEach(rk => {
-  if (tranv2(rk, false)) {
-    console.log('found', rk)
-  }
-})
+// '35S'
+const root =  'T84' // 'K4C' // 'NHG' // '1XS' // '2S3' // 'CG2' // '9PN' // 'YPD' // '3MR' // 'NS9' // 'SCX' // '439' // 'W9W' // 'CW6' // 'JJ2' // 'C66' // '4T3' // 'T8V' // '4JG' // 'S96' // 'VK6' // 'TPP' // 'PMD' // 'Q63' // 'WDK' // 'LXJ' // 'FPL' 
+// 'CT7' // 'WXJ' // '9QY' // 'PDX' // 'ZZZ' 
+// 'ZVM' // 'BL5' // 'NCP' // 'WF3' // '1LQ' // 'Z27' // 'KMJ' // 'R72' 
+// 'B7R' // 'WBH' // 'VQT' // 'RQT' // '8VF' // 'CGL' // 'B9W' 
+// '6GC' // 'S3N' // '6R3' // '7CM' // '751' // 'XH1' 
+// '92R' // 'R3L' // 'F7V' // '1VG' // 'CMZ'
+// 'Z7D' // 'HFV' // '2KZ' // 'XZQ' // 'SNN' 
+// 'PVY' // 'J2L' // '1PX' // 'K9X' // 'COM'
 
+console.log('Day 6-2: YOU/SAN', 
+  tranv2(root, 'YOU', false, 0),
+  tranv2(root, 'SAN', false, 0)
+)
 
-// console.log('Day 5-2:', '')
+console.log('Day 6-2:', 
+
+)
+
+// COM, K9X
