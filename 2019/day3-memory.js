686c2684d692b572876fd992eb2c5e577261f9c5
Fang <windmaomao@gmail.com>
2019-12-09 17:04:38 -0500

    find two lines

--- 2019/day3-memory.js
+++ 2019/day3-memory.js
@@ -1,36 +1,44 @@
 const filereader = require('./utils/filereader.js')
-const lines = filereader.readFile('/day3.data')
+const data = filereader.readFile('/day3.data')
 
-// const _sparse = () => {
-//   const arr = {}
-//   const _pos = (i, j) => 'i'+i+'j'+j
-//   const append = (i, j, v) => {
-//     arr[_pos(i, j)] = v
-//   }
-//   const exist = (i, j) => {
-//     return true
-//   }
-
-//   return { arr, append, exist }
-// }
-
-const memories = lines.map(line => {
+const lines = data.map(line => {
   const parts = line.split(',')
   const o = { x: 0, y: 0 }
 
   return parts.reduce((acc, ins) => {
     const char = ins[0]
     const num = parseInt(ins.substring(1))
-    switch (char) {
-      case 'R': o.x += num; break
-      case 'L': o.x -= num; break
-      case 'U': o.y += num; break
-      case 'D': o.y -= num; break
+    const next = (dx, dy) => {
+      o.x += dx; o.y += dy; acc.push({...o})
     }
-    acc.push({...o})
+    for (let i = 1; i <= num; i++) {
+      switch (char) {
+        case 'R': next(1, 0); break
+        case 'L': next(-1, 0); break
+        case 'U': next(0, 1); break
+        case 'D': next(0, -1); break
+      }
+    }
     return acc
   }, [])
-
 })
 
-console.log(memories[0])
+console.log(lines[0].length, lines[1].length)
+
+let prev = { x: 0, y: 0 }, filter = { x: [], y: [] }
+const intersect = (arr1, arr2) => {
+  arr1.filter(a1 => {
+    // console.log(a1)
+    if (a1.x != prev.x) filter.x = arr2.filter(a2 => a2.x == a1.x)
+    if (a1.y != prev.y) filter.y = arr2.filter(a2 => a2.y == a1.y)
+
+    const found = filter.x.filter(b1 => {
+      return filter.y.filter(b2 => {
+        b1.x == b2.x && b1.y == b2.y
+      })
+    })
+    // console.log(found)
+  })
+}
+
+console.log(intersect(lines[0], lines[1]))
