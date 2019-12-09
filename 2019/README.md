fe6797f7bd25a7fd97295e40cff170e4ad9a2c72
Fang <windmaomao@gmail.com>
2019-12-09 13:04:17 -0500

    clean and add reame

---
+++ 2019/README.md
@@ -0,0 +1,31 @@
+## Stats
+
+```
+      --------Part 1--------   --------Part 2--------
+Day       Time   Rank  Score       Time   Rank  Score
+  9   01:45:38   2096      0   01:47:26   2067      0
+  2   02:48:52   6619      0   02:56:45   5582      0
+  1   17:41:17  25487      0   18:17:49  23501      0
+```
+
+## Day 1
+
+Coming back from the thanks giving trip at night, I was curious to know what is Advent Code at around 5:40pm. I joined it with my github account, and finished day 1 with about half a hour. My skillset is pretty rough, spend half time in setting up the infrastracture code, ex. reading the input.
+
+## Day 2
+
+Waked up in the middle of night at 3am, I was excited to checkout day 2. The question was pretty interesting to read and work on, although I'm sure it might be a bit difficult to non-english speaking audience. I finished in about an hour, and was very shocked by how much time I spent on unnecceary thinking and work.
+
+## Day 9
+
+Fully prepared once again to finish day 9 and still spent almost 2 hours. While improving day 5 code (relative simple version of day 7 code), the problem I stuck with 
+
+- Forgot some term from the past days
+- Made a mistake in setting address vs value
+
+This time the judge made a first time attempt of ommiting some details and allowing you figure them out. For instance, the third parameter it introduced requires same parameters which is not handled or even revealed by the old version. Lessor learned
+
+- Pay attention to question details, read slow
+- Print out key step and make them clear and clean
+
+Though the question is tougher, I managed to hit the ranking around 2000 first time. Also first time I managed to spend very small time (2mins) on part 2. 
--- 2019/day9_intcode.js
+++ 2019/day9_intcode.js
@@ -1,7 +1,3 @@
-const filereader = require('./utils/filereader')
-const data = filereader.readFile('/day9.data', ',', true)
-const intcode = require('./day9_intcode')
-
 const defaultOptions = {
   once: false,
   i: 0,
@@ -9,30 +5,32 @@ const defaultOptions = {
   output: true
 }
 
-const compDayStates = (arr, signal, ops) => {
+const _digit = (op, pos) => (op / 2 ^ pos) % 10 | 0
+
+const intcode = (arr, signal, ops) => {
   const options = Object.assign({}, defaultOptions, ops)
-  let i = 0
-  let done = false
-  let relative = 0
   let output = signal
+  let done = false
+  let i = options.i
+  let relative = 0
 
   const _v = (arr, pos) => pos < arr.length ? (
-    arr[pos] === undefined ? 0 : arr[pos]
-  ) : 0
+      arr[pos] === undefined ? 0 : arr[pos]
+    ) : 0
 
-  const val = (dif, flag) => !flag ? _v(arr, _v(arr, i + dif)) :
+  const val = (dif, flag) => !flag ? _v(arr, _v(arr, i + dif)) : 
     (flag == 1 ? _v(arr, i + dif) : _v(arr, _v(arr, i + dif) + relative))
 
   const sto = (dif, v, flag) => {
-    !flag ? (arr[_v(arr, i + dif)] = v) :
-      (arr[_v(arr, i + dif) + relative] = v)
+    if (!flag) arr[_v(arr, i + dif)] = v; 
+    else arr[_v(arr, i + dif) + relative] = v
   }
 
   const inc = (dif, flag) => { i = !flag ? i + dif : dif }
 
-  const prt = v => {
-    output = v;
-    options.output && console.log('Output', v)
+  const prt = v => { 
+    output = v; 
+    options.output && console.log('Output', v) 
   }
 
   const rel = dif => {
@@ -43,10 +41,10 @@ const compDayStates = (arr, signal, ops) => {
   do {
     const op = arr[i]
     const bp = op % 100
-    const f1 = (op / 100) % 10 | 0
-    const f2 = (op / 1000) % 10 | 0
-    const f3 = (op / 10000) % 10 | 0
-    // console.log('OP', f2, f1, bp, arr.length)
+
+    const f1 = _digit(bp, 2), f2 = _digit(bp, 3), f3 = _digit(bp, 4)
+    options.output && console.log('OP', f2, f1, bp)
+
     switch (bp) {
       case 1: sto(3, val(1, f1) + val(2, f2), f3); inc(4); break
       case 2: sto(3, val(1, f1) * val(2, f2), f3); inc(4); break
@@ -61,14 +59,8 @@ const compDayStates = (arr, signal, ops) => {
       case 99: done = true; break;
     }
   } while (!done)
-  return true
+  if (options.once) return false
+  return output
 }
 
-console.log('Day 9/1:', compDayStates([...data], 1))
-// INPUT = 2
-// console.log('Day 9/2:', compDayStates([...data]))
-
-// const data = [104, 1125899906842624, 99]
-// const data = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
-// const data = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
-
+module.exports = intcode
