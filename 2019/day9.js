b15b593b985eb22c45ed26fd33963be8242ee9d2
Fang <windmaomao@gmail.com>
2019-12-09 13:25:02 -0500

    clean day9

--- 2019/day9.js
+++ 2019/day9.js
@@ -2,11 +2,9 @@ const filereader = require('./utils/filereader')
 const data = filereader.readFile('/day9.data', ',', true)
 const intcode = require('./day9_intcode')
 
-console.log('Day 9/1:', intcode([...data], 1, { verbose: true }))
-// INPUT = 2
-// console.log('Day 9/2:', compDayStates([...data]))
+console.log('Day 9/1:', intcode([...data], 1, { verbose: false }))
+console.log('Day 9/2:', intcode([...data], 2, { verbose: false }))
 
 // const data = [104, 1125899906842624, 99]
 // const data = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
 // const data = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
-
