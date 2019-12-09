b30815c376cc5adf65920c9a6cc965df2715843d
Fang <windmaomao@gmail.com>
2019-12-09 00:21:38 -0500

    add code

--- 2019/day9.js
+++ 2019/day9.js
@@ -1,4 +1,61 @@
 const filereader = require('./utils/filereader')
-const [data] = filereader.readFile('/day9.data')
+// const data = filereader.readFile('/day9.data', ',', true)
+// const data = [104, 1125899906842624, 99]
+// const data = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
+const data = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
 
-console.log('Day 9/1:', data)
+const INPUT = 1
+
+const compDayStates = arr => {
+  let i = 0
+  let done = false
+  let relative = 0
+
+  const val = (dif, flag) => !flag ?
+    arr[arr[i + dif]] : (flag == 1 ? 
+      arr[i + dif] : arr[i + dif + relative])
+
+  const sto = (dif, v, flag) => {
+    arr[arr[i + dif]] = v
+  }
+
+  const inc = (dif, flag) => {
+    i = !flag ? i + dif : dif
+  }
+
+  const prt = v => { 
+    if (v === 99) done = true 
+    console.log('Output', v) 
+  }
+
+  const rel = (dif) => {
+    relative += dif
+    console.log('Relative', relative)
+  }
+
+  do {
+    const op = arr[i]
+    const bp = op % 100
+    const f1 = (op / 100) % 10 | 0
+    const f2 = (op / 1000) % 10 | 0
+    console.log('OP', f2, f1, bp)
+    switch (bp) {
+      case 1: sto(3, val(1, f1) + val(2, f2)); inc(4); break
+      case 2: sto(3, val(1, f1) * val(2, f2)); inc(4); break
+      case 3: sto(1, INPUT); inc(2); break
+      case 4: prt(val(1, f1)); inc(2); break
+      case 5: val(1, f1) ? inc(val(2, f2), 1) : inc(3); break
+      case 6: !val(1, f1) ? inc(val(2, f2), true) : inc(3); break
+      case 7: sto(3, val(1, f1) < val(2, f2) ? 1 : 0); inc(4); break
+      case 8: sto(3, val(1, f1) === val(2, f2) ? 1 : 0); inc(4); break
+      case 9: rel(val(1, f1)); inc(2); break
+      default:
+      case 99: done = true; break;
+    }
+  } while (!done)
+  return arr
+}
+
+const day91States = [...data]
+
+console.log('Day 9/1:', compDayStates(day91States))
