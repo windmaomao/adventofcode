f52c41a6e6dac9dfb74e9eb23fab301b80f8f2c4
Fang <windmaomao@gmail.com>
2019-12-11 16:55:03 -0500

    add utils for plot

--- 2019/README.md
+++ 2019/README.md
@@ -6,9 +6,9 @@ C - Counting, H - Hashmap, I - Intcode
 
                   1 1 1 1 1 1 1 1
 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7
-C I H C I H I C I H I
-    C     C       C H
-                    C 
+C   C C   C   C   C C
+    H     H       H H
+  I     I   I   I   I 
 
 ## Stats
 
---
+++ 2019/utils/plot.js
@@ -0,0 +1,42 @@
+const MIN = -100000
+const MAX = 1000000
+
+const _init = () => ({ min: MAX, max: MIN })
+
+function plot(arr) {
+  const bound = arr.reduce((acc, p) => {
+    if (p.x < acc.x.min) acc.x.min = p.x
+    if (p.y < acc.y.min) acc.y.min = p.y
+    if (p.x > acc.x.max) acc.x.max = p.x
+    if (p.y > acc.y.max) acc.y.max = p.y
+    return acc
+  }, { x: _init(), y: _init() })
+
+  const dx = -bound.x.min, dy = -bound.y.min
+  const xc = bound.x.max - bound.x.min + 1
+  const yc = bound.y.max - bound.y.min + 1
+
+  const pic = new Array(yc).fill(' ').map(_ => (
+    new Array(xc).fill(' ')
+  ))
+
+  arr.forEach(p => {
+    const x = p.x + dx, y = p.y + dy
+    pic[y][x] = '*'
+  })
+
+  const drawing = pic.reverse().map(row => row.join('')).join('\n')
+
+  return { pic, bound, drawing }
+}
+
+module.exports = plot
+
+// const arrs = [
+//   { x: 3, y: 3 }, 
+//   { x: 1, y: 5 },
+//   { x: -2, y: 4 },
+//   { x: -10, y: 10}
+// ]
+// const pic = plot(arrs)
+// console.log(pic.drawing)
