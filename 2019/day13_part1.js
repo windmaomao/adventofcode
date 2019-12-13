27e8f8a9fc4706efef60ae20ca28ce2964519dff
Fang <windmaomao@gmail.com>
2019-12-13 17:05:53 -0500

    clean and init board

--- 2019/day13_part1.js
+++ 2019/day13_part1.js
@@ -3,35 +3,32 @@ const raw = filereader.readFile('day13.data', ',', true)
 const play = require('./day13_play')
 const debug = require('debug')('day13:')
 
+const initBoard = (auto) => {
+  const pos = new Array(21).fill(0).map(row => new Array(44).fill(' '))
+  return {
+    once: true, print: false,
+    output: 0, i: 0, relative: 0, score: 0, done: false,
+    pos, bar: { x: 0, y: 0 }, ball: { x: 0, y: 0 }, auto
+  }
+}
+
 const plotBoard = (board) => {
   const picture = board.pos.map(row => row.join('')).join('\n')
   debug(picture)  
 }
 
+const raw1 = [...raw]
 const runBoard = (data) => {
-  const pos = new Array(21).fill(0).map(row => new Array(44).fill(' '))
-  let board = {
-    once: true, print: false,
-    output: 0, i: 0, relative: 0, score: 0, done: false,
-    pos, bar: { x: 0, y: 0 }, ball: { x: 0, y: 0 }, auto: false
-  }
+  let board = initBoard(false)
   board = play(data, board)
   plotBoard(board)
 }
 
-debug('Day 13/1')
-runBoard([...raw])
+debug('Day 13/1'); runBoard(raw1)
 
-const raw2 = [...raw]
-raw2[0] = 2
-
+const raw2 = [...raw]; raw2[0] = 2
 const runBoard2 = (data) => {
-  const pos = new Array(21).fill(0).map(row => new Array(44).fill(' '))
-  let board = {
-    once: true, print: false,
-    output: 0, i: 0, relative: 0, score: 0, done: false,
-    pos, bar: { x: 0, y: 0 }, ball: { x: 0, y: 0 }, auto: false
-  }
+  let board = initBoard(false)
   const moves = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1,
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 1, 1, 1, 0, 0, 
@@ -46,8 +43,31 @@ const runBoard2 = (data) => {
   debug('score:', board.score)
 }
 
-debug('Day 13/2')
-runBoard2(raw2)
+debug('Day 13/2a - Manual'); runBoard2(raw2)
+
+// const runBoard3 = (data) => {
+//   const pos = new Array(21).fill(0).map(row => new Array(44).fill(' '))
+//   let board = {
+//     once: true, print: false,
+//     output: 0, i: 0, relative: 0, score: 0, done: false,
+//     pos, bar: { x: 0, y: 0 }, ball: { x: 0, y: 0 }, auto: false
+//   }
+//   const moves = [
+//     0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+//     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 1, 1, 1, 0, 0,
+//     0, 0, 0, 0, 0, 0, 0, 1
+//   ]
+
+//   moves.forEach(m => {
+//     board = play(data, board)
+//     board.output = m
+//   })
+//   plotBoard(board)
+//   debug('score:', board.score)
+// }
+
+// debug('Day 13/2b - Auto')
+// runBoard2(raw2)
 
 
 
