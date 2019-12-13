364ce9477a1332f3f0f11624141d7883eff877c0
Fang <windmaomao@gmail.com>
2019-12-12 20:08:30 -0500

    refine

--- 2015/src/1/index.js
+++ 2015/src/1/index.js
@@ -1,7 +1,15 @@
 import raw from './input'
 
+const _m = { '(': 1, ')': -1 }
+const floor = arr => arr.reduce((acc, a, i) => {
+  const res = acc + _m[a]
+  if (res === -1) { console.log(i) }
+  return acc + _m[a]
+}, 0)
+
 const day1 = () => {
-  console.log(raw)
+  console.log(floor(raw.split('')))
+  // console.log(raw.split('').length)
 }
 
 export default day1
