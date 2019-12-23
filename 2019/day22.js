c2b32fc86f31941919d7118c1838a2332cb0b99e
Fang <windmaomao@gmail.com>
2019-12-22 19:47:31 -0500

    clean js

--- 2019/day22.js
+++ 2019/day22.js
@@ -7,25 +7,71 @@ const moduleInverse = (pos, size, modBy) => {
   while (last != 0) {
     last -= modBy
     if (last < 0) last += size
-    i++   
+    i++
   }
   return i
 }
 
-const shufflePos = (ops, size, pos) => {
+const moduleInverse2 = (pos, size, modBy) => {
+  let i = 0, last = pos, newLast = -1
+  while (newLast != 0) {
+    newLast = last % modBy
+    i += (last - newLast) / modBy
+    last = newLast + size
+  }
+  return i
+}
+
+const moduleInverse3 = (a, n) => {
+  let t = 0, newt = 1
+  let r = n, newr = a
+  let q, oldt, oldr
+  while (newr != 0) {
+    q = r / newr | 0
+    oldt = newt; newt = t - q * oldt; t = oldt;
+    oldr = newr; newr = r - q * oldr; r = oldr;
+  }
+  return r > 1 ? null : (t < 0 ? t + n : t) % n
+}
+
+const moduleForward = (pos, size, modBy) => {
+  let last = pos, i = 0
+  while (last > 0) {
+    i += modBy
+    if (i > size - 1) i -= size
+    last--
+  }
+  return i
+}
+
+const bigOp = (a, b, c) => (BigInt(a) * BigInt(b)) % BigInt(c)
+
+const shufflePos = (ops, size, pos, backward = false) => {
   let last = pos
-  ops.reverse().forEach(op => {
-    switch(op[0]) {
-      case 'i':   // deal with increment
-        last = moduleInverse(last, size, op[1])
-        break;
+  const list = !backward ? [...ops] : ops.reverse()
+  list.forEach(op => {
+    debug(op)
+    switch (op[0]) {
       case 'n':   // deal into new stack
-        last = size -1 - last
+        last = size - 1 - last
+        break;
+      case 'i':   // deal with increment
+        if (!backward) {
+          last = moduleForward(last, size, op[1])
+        } else {
+          // last = moduleInverse2(last, size, op[1])
+          last = bigOp(last, moduleInverse3(op[1], size), size)
+        }
         break;
       case 'c':   // cut
-        last = (last + op[1] + size) % size
+        if (!backward) {
+          last = (last - op[1] + size) % size
+        } else {
+          last = (last + op[1] + size) % size
+        }
         break;
     }
+    debug(last)
   })
   return last
 }
@@ -37,9 +83,14 @@ const orders = raw.map(line => {
   return ['n']
 })
 
-debug(shufflePos(orders, 10007, 2020))
+debug('Part 1:', shufflePos(orders, 10007, 2019))
 
-// debug(shufflePos(orders, 10, 9))
+const rOrders = [
+  ['i', 14722883731704],
+  ['c', 105269438226243],
+  ['n']
+]
+debug('Part 2:', shufflePos(rOrders, 119315717514047, 2020, true))
 
 // const testOrders1 = [
 //   ['i', 7],
@@ -48,9 +99,9 @@ debug(shufflePos(orders, 10007, 2020))
 // ]
 
 // const testOrders2 = [
-//   ['cut', 6],
-//   ['increment', 7],
-//   ['new stack'],
+//   ['c', 6],
+//   ['i', 7],
+//   ['n'],
 // ]
 
 // const testOrders4 = [
