87d8dcfeaa2e168ff43d313f0bfc21bf06b1a0e0
Fang <windmaomao@gmail.com>
2019-12-25 17:50:26 -0500

    add day5 ruby

--- 2019/rb/day5.rb
+++ 2019/rb/day5.rb
@@ -1,6 +1,6 @@
 require_relative 'utils/intcode'
 
-input = ARGF.read.split(?,).map(&:to_i)
+fn = open("2019/inputs/day5.data")
 
 def run(mem, input, ops) 
   b = Intcode.new(mem, valid_ops: ops)
@@ -8,5 +8,6 @@ def run(mem, input, ops)
   b.output
 end
 
+input = fn.read.split(?,).map(&:to_i)
 puts run(input, 1, [1,2,3,4,99])[-1]
 puts run(input, 5, [1,2,3,4,5,6,7,8,99])[-1]
--- 2019/rb/index.rb
+++ 2019/rb/index.rb
@@ -1,4 +1,5 @@
 # require_relative 'day1'
 # require_relative 'day2'
 # require_relative 'day3'
-require_relative 'day4'
+# require_relative 'day4'
+require_relative 'day5'
