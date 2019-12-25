114fb574b4ebcb0e30baca5eabd2ca6bc8bc4aee
Fang <windmaomao@gmail.com>
2019-12-25 12:01:45 -0500

    refine day2 ruby

--- 2019/rb/day2.rb
+++ 2019/rb/day2.rb
@@ -3,11 +3,12 @@ require_relative 'utils/intcode'
 input = ARGF.read.split(?,).map(&:to_i)
 
 def run(mem, noun, verb)
-  mem[1] = noun
-  mem[2] = verb
-  b = Intcode.new(mem, valid_ops: [1, 2, 99])
-  b.continue
-  return b.memory[0]
+  src = mem.dup
+  src[1, 2] = [noun, verb]
+  Intcode
+    .new(src, valid_ops: [1, 2, 99])
+    .continue
+    .memory[0]
 end
 
 puts run(input, 12, 2)
