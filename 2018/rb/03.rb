ca0c98e2f1657f95b27b2e90303f7879a81086a7
Fang <windmaomao@gmail.com>
2020-01-02 21:36:18 -0500

    clean input

--- 2018/rb/03.rb
+++ 2018/rb/03.rb
@@ -6,9 +6,6 @@ def splitNums(str)
   str.split(/[# @,:x]/).delete_if(&:empty?).map(&:to_i)
 end
 
-input = fn.each_line.map(&:chomp).map{|s| splitNums(s)}
-
-# Part 1
 def fabricPts(x, y, w, h)
   m = []
   w.times do |i|
@@ -19,25 +16,25 @@ def fabricPts(x, y, w, h)
   m
 end
 
-fabrics = input.map{|f| fabricPts(f[1],f[2],f[3],f[4])}
-fm = {}
+input = fn.each_line.map(&:chomp).map{|s| 
+  i, x, y, w, h = splitNums(s)
+  fabricPts(x, y, w, h)
+}
 
-N = 1000*1000
-fabrics.each{ |pts|
+# Part 1
+fm = Hash.new { |h, k| h[k] = 0 }
+
+input.each{ |pts|
   pts.each{ |pt| 
-    x, y = pt
-    c = x * N + y
-    fm[c] = fm[c] ? fm[c] + 1 : 1
+    fm[pt] += 1
   }
 }
 
 p fm.count{ |_, v| v > 1 }
 
 # Part 2
-p fabrics.index{|pts| 
+p input.index{|pts| 
   pts.all? { |pt|
-    x, y = pt
-    c = x * N + y
-    fm[c] == 1
+    fm[pt] == 1
   }
 } + 1
