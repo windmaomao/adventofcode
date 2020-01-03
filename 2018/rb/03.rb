0f3c1737777b47f80fbbb1f41ee201cc97ac28d2
Fang <windmaomao@gmail.com>
2020-01-02 21:25:51 -0500

    day3 part 2

--- 2018/rb/03.rb
+++ 2018/rb/03.rb
@@ -1,7 +1,12 @@
 require 'set'
 
 fn = open("2018/input/03")
-input = fn.each_line.map(&:chomp)
+
+def splitNums(str)
+  str.split(/[# @,:x]/).delete_if(&:empty?).map(&:to_i)
+end
+
+input = fn.each_line.map(&:chomp).map{|s| splitNums(s)}
 
 # Part 1
 def fabricPts(x, y, w, h)
@@ -14,15 +19,12 @@ def fabricPts(x, y, w, h)
   m
 end
 
-
-fabrics = []
-fabrics << fabricPts(1, 3, 4, 4)
-fabrics << fabricPts(3, 1, 4, 4)
+fabrics = input.map{|f| [f[0], fabricPts(f[1],f[2],f[3],f[4])]}
 fm = {}
 
 N = 1000*1000
 fabrics.each{ |pts|
-  pts.each{ |pt| 
+  pts[1].each{ |pt| 
     x, y = pt
     c = x * N + y
     fm[c] = fm[c] ? fm[c] + 1 : 1
@@ -32,3 +34,10 @@ fabrics.each{ |pts|
 p fm.count{ |_, v| v > 1 }
 
 # Part 2
+p fabrics.index{|pts| 
+  pts[1].all? { |pt|
+    x, y = pt
+    c = x * N + y
+    fm[c] == 1
+  }
+} + 1
