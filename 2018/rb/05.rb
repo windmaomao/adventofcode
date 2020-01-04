8b55cd11553007ca2bcc2329a8972eabbd8217f9
Fang <windmaomao@gmail.com>
2020-01-04 16:48:58 -0500

    add day5

--- 2018/rb/05.rb
+++ 2018/rb/05.rb
@@ -1,16 +1,31 @@
-input = "dabAcCaCBAcCcaDA".chars
+fn = open("2018/input/05")
+input = fn.each_line.map(&:chomp)[0].bytes.to_a.freeze
+# input = "dabAcCaCBAcCcaDA".bytes.to_a.freeze
 
-def react(list)
+C = 32
+def reduct(list)
   i = 0
   while i < list.size - 1
-    if (list[i].ord - list[i+1].ord).abs == 32
+    dif = list[i] - list[i+1]
+    if dif == C || dif == -C
       list[i, 2] = []
       i = 0
     else
       i += 1
     end
   end
-  list
+  list.size
 end
 
-p react(input.dup).size
+# Part 1
+# p reduct(input.dup)
+
+# Part 2
+lens = ("A".."Z").to_a.map(&:ord).map{|o| 
+  remove = input.dup.select{ |c| (c != o && c!= o + C )}
+  reduced = reduct(remove)
+  p "#{o.chr} #{reduced}"
+  reduced
+}
+
+p lens.min
