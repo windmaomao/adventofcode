1a650028db9a6ca4517b45a0b1c2f16ae5854aa2
Fang <windmaomao@gmail.com>
2020-01-04 17:08:22 -0500

    update day5 using strings

--- 2018/rb/05.rb
+++ 2018/rb/05.rb
@@ -1,31 +1,27 @@
 fn = open("2018/input/05")
-input = fn.each_line.map(&:chomp)[0].bytes.to_a.freeze
-# input = "dabAcCaCBAcCcaDA".bytes.to_a.freeze
+input = fn.each_line.map(&:chomp)[0].freeze
+# input = "dabAcCaCBAcCcaDA".freeze
 
-C = 32
+def squeeze!(list)
+  ("a".."z").each { |c| 
+    list.gsub!("#{c}#{c.upcase}", "")
+    list.gsub!("#{c.upcase}#{c}", "")
+  }
+end
+
 def reduct(list)
-  i = 0
-  while i < list.size - 1
-    dif = list[i] - list[i+1]
-    if dif == C || dif == -C
-      list[i, 2] = []
-      i = 0
-    else
-      i += 1
-    end
+  loop do 
+    size = list.size
+    squeeze!(list)
+    break if size == list.size
   end
   list.size
 end
 
 # Part 1
-# p reduct(input.dup)
+p reduct(input.dup)
 
 # Part 2
-lens = ("A".."Z").to_a.map(&:ord).map{|o| 
-  remove = input.dup.select{ |c| (c != o && c!= o + C )}
-  reduced = reduct(remove)
-  p "#{o.chr} #{reduced}"
-  reduced
-}
-
-p lens.min
+p ("a".."z").map{|c| 
+  reduct(input.gsub(/#{c}/i, ""))
+}.min
