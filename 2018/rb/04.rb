905e4bd5fb6a8154a45ad3b1d2aaeef3bde4e7c6
Fang <windmaomao@gmail.com>
2020-01-04 10:13:51 -0500

    add day4 new version

--- 2018/rb/04.rb
+++ 2018/rb/04.rb
@@ -1,75 +1,38 @@
-require 'set'
-
 fn = open("2018/input/04")
+input = fn.each_line.map(&:chomp).map(&:freeze).freeze
 
-def parseLine(str)
-  sp = str.split("] ")
-  datetime = sp[0][1, 16]
-  dp = datetime.split(" ")
-  date = dp[0]
-  hp = dp[1].split(":")
-  hour, minute = hp
-  action = case sp[1]
-  when "falls asleep"
-    ["", "sleep"]
-  when "wakes up"
-    ["", "wake"]
-  else
-    pp = sp[1].split(/[# @,:x]/)
-    [pp[2], "start"]
-  end
-  [date, hour, minute] + action
+def lastInt(l)
+  Integer(l.scan(/\d+/).last, 10)
 end
 
-guards = Hash.new { |h, k| h[k] = [] }
-
-input = fn.each_line.map(&:chomp).map{|str| 
-  date, hour, minute, id, action = parseLine(str)
-  [date, hour, minute, id, action]
-}.sort_by{ |l| 
-  l[0]+l[1]+l[2] 
-}.slice_before{ |l| 
-  !l[3].empty? 
+chunks = input.sort.slice_before{ |l| 
+  l.end_with?('begins shift') 
 }.to_a
 
-# Part 1
-def sleepTime(arr)
-  # arr.each{|r| p r}
-  sleeps = Array.new(60, 0)
+N = 60
+guards = Hash.new { |h, k| h[k] = Array.new(N, 0) }
+chunks.each{ |arr| 
+  guard = lastInt(arr[0])
   arr.each_cons(2) { |p| 
-    if (p[0][4] == 'sleep') 
-      i = p[0][2].to_i
-      j = p[1][2].to_i - 1
-      (i..j).each{|k| sleeps[k] = 1}
+    if p[0].end_with?('falls asleep')
+      i, j = p.map{|l| Integer(l.scan(/\d+/).last, 10)}
+      (i..j-1).each{|k| guards[guard][k] += 1}
     end
   }
-  # p sleeps.map{|x| x > 0 ? '#' : '.'}.join('')
-  sleeps
-end
-
-def popularTime(arr) 
-  res = Array.new(60, 0).map.with_index{ |_, i| 
-    arr.inject(0) { |acc, a| acc + a[i] }
-  }
-  res.each_with_index.max
-end
-
-guards = Hash.new{ |h, k| h[k] = {
-  "id" => 0,
-  "days" => [],
-  "slepts" => 0
-}}
-input.map{|g| 
-  id = g[0][3].to_i
-  time = sleepTime(g)
-  guards[id]["days"].push(time)
-  guards[id]["slepts"] += time.inject(&:+)
 }
-lazyFound = guards.max_by{|k, v| v["slepts"] }
-p lazyFound[0]
-p popularTime(lazyFound[1]["days"])[1]
+
+# Part 1
+guardId, minutes = guards.max_by{|k, v| v.sum }
+high, minuteId = minutes.each_with_index.max
+p guardId * minuteId
 
 # Part 2
-lazyFound2 = guards.max_by{|k, v| popularTime(v["days"])[0] }
-p lazyFound2[0]
-p popularTime(lazyFound2[1]["days"])[1]
+guardId, minutes = guards.max_by{|k, v| v.max }
+high, minuteId = minutes.each_with_index.max
+p guardId * minuteId
+
+
+# def printGuard(i, g)
+#   p i, g.map{|x| x > 0 ? '#' : '.'}.join('')
+# end
+# guards.each{ |k, v| printGuard(k, v) }
