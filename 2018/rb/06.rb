f5518358c57bdfb2de8aeab86445cd56702ff3d9
Fang <windmaomao@gmail.com>
2020-01-04 17:50:15 -0500

    plot day 6

--- 2018/rb/06.rb
+++ 2018/rb/06.rb
@@ -1,6 +1,18 @@
-fn = open("2018/input/06")
+fn = open("2018/input/06a")
 input = fn.each_line.map(&:chomp).map{|l|
   l.split(", ").map(&:to_i).freeze
 }.freeze
 
-p input
+# (0..1).map{ |i|
+#   p input.map{|v| v[i]}.minmax
+# }
+
+N = 9
+puts (0..N).map{ |y|
+  (0..N).map{ |x|
+    dist = input.map{ |c| (x - c[0]).abs + (y - c[1]).abs }
+    minDist, closeId = dist.each_with_index.min
+    isUniq = dist.select{ |v| v == minDist }.size == 1
+    isUniq ? (closeId+65).chr : '.'
+  }.join('')
+}
