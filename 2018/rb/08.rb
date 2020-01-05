7e142c7153f12f4dbb0936d0bd6e11eedcd54e5c
Fang <windmaomao@gmail.com>
2020-01-05 15:36:45 -0500

    finish day8 part2

--- 2018/rb/08.rb
+++ 2018/rb/08.rb
@@ -1,26 +1,45 @@
 fn = open("2018/input/08")
-# input = fn.each_line.map(&:chomp)[0].split(" ")
-input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".split(" ").map(&:to_i)
+input = fn.each_line.map(&:chomp)[0].split(" ").map(&:to_i)
+# input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".split(" ").map(&:to_i)
 
+# Part 1
 i = 0
 stack = []
-values = []
+value = 0
 
 while i < input.size
-  node = [input[i], input[i+1]]
+  node = [input[i], input[i+1], []]
   stack << node
   i += 2
 
   loop do
-    p stack
+    # p stack
     break if stack.size < 1
     curr = stack.last
     curr[0] -= 1
     break if curr[0] >= 0
-    index, count = stack.pop
-    values += input[i, count]
-    i += count
+
+    # update the node meta
+    _, metaCount, children = stack.pop
+    
+    value = 0
+    childCount = children.size
+    if (childCount > 0)
+      (1..metaCount).each { |j| 
+        index = input[i + j - 1] - 1
+        value += index < childCount ? children[index] : 0
+      }
+    else
+      value = input[i, metaCount].inject(&:+)
+    end
+
+    # p value
+    if (stack.last)
+      stack.last[2] << value
+    end
+
+    i += metaCount
   end
 end
 
-p values.inject(&:+)
+p value
