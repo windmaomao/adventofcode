e80aa1379aebc2bcb6ae9dee4a4fedc442a69fea
Fang <windmaomao@gmail.com>
2020-01-05 12:34:03 -0500

    add working part2

--- 2018/rb/07b.rb
+++ 2018/rb/07b.rb
@@ -15,9 +15,53 @@ def noDepList(order)
 end 
 
 # Part 1
-order = []
-loop do
-  order << (noDepList(order) - order).sort.first
-  break if order.size == Deps.size
+
+orders = []
+while orders.size < Deps.size
+  orders << (noDepList(orders) - orders).sort.first
 end
-p order
+# p orders
+
+# Part 2
+
+time = 0
+working = [["", 0], ["", 0], ["", 0], ["", 0], ["", 0]]
+orders = []
+
+while orders.size < 1
+  # no dependency, not in order, not in working
+  canWork = noDepList(orders).select{ |v| 
+    !orders.include?(v) &&
+    !working.map(&:first).include?(v)
+  }
+
+  p canWork
+
+  working.each { |w|
+    workingOn, _ = w
+    if canWork.size > 0 && workingOn == ""
+      order = canWork.sort.first
+      canWork.delete(order)
+      w[0, 2] = [order, order.ord - 4]
+    end
+  }
+
+  time += 1
+
+  working.each { |w|
+    workingOn, timeLeft = w
+    next if workingOn == ""
+
+    timeLeft -= 1
+    if (timeLeft == 0)
+      orders << workingOn
+      w[0, 2] = ["", 0]
+    else
+      w[0, 2] = [workingOn, timeLeft]
+    end
+  }
+
+  # p canWork
+  p working
+end
+# p orders
