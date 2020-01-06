ba1e3a7f2d79ab14a9ed5cd2ce40da7a618732b5
Fang <windmaomao@gmail.com>
2020-01-05 19:26:30 -0500

    day 9 part 1

--- 2018/rb/09.rb
+++ 2018/rb/09.rb
@@ -1,16 +1,26 @@
 
-N = 9
-players = Array.new(9, 0)
+N = 428
+M = 72061*3
+players = Array.new(N, 0)
 curr = 0
 marble = 0
 marbles = [0]
 
-while marble < 22
+while marble < M
   marble += 1
-  i = (curr + 1) % marbles.size
-  marbles[i, 1] = [marbles[i], marble]
-  curr = i + 1
+  if (marble % 23 == 0)
+    i = (curr - 7) % marbles.size
+    players[marble % N] += marble + marbles[i]
+    marbles[i, 1] = []
+    curr = i
+  else
+    i = (curr + 1) % marbles.size
+    marbles[i, 1] = [marbles[i], marble]
+    curr = i + 1
+  end
+  print '.' if marble % 100000 == 0
 end
 
 p marble % N
-p marbles.map.with_index{|v, i| " #{v}" + ((i == curr) ? ")" : "")}.join
+# p marbles.map.with_index{|v, i| " #{v}" + ((i == curr) ? ")" : "")}.join
+p players.max
