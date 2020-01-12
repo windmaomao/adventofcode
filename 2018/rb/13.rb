7dfeb840cd0a9711f969d070c0d99b917deb3b25
Fang <windmaomao@gmail.com>
2020-01-12 15:34:56 -0500

    refine

--- 2018/rb/13.rb
+++ 2018/rb/13.rb
@@ -5,23 +5,19 @@ carts = []
 symbols = ['<', '>', '^', 'v']
 input.each_with_index{|l, y|
   l.chars.each_with_index{|c, x| 
-    found = symbols.index(c)
-    next if found == nil
-    carts << {
-      "pos" => [x, y],
-      "dir" => found,
-      "count" => 0 
-    }
-    l[x] = found > 1 ? '|' : '-'
+    dir = symbols.index(c)
+    next if dir == nil
+    carts << [[x, y], dir, 0]
+    l[x] = dir > 1 ? '|' : '-'
   }
 }
 
 # Part 1
-def move(p, d, m)
-  x, y = p
+def move(cart, m)
+  (x, y), d = cart
   c = m[y][x]
   # p c,d
-  normals = [[x-1,y,d], [x+1,y,d], [x,y-1,d], [x,y+1,d]]
+  normals = [[x-1,y,d], [x+1,y,d], [x,y-1,d], [x,y+1,d]]  # - or |
   lefts =   [[x,y+1,3], [x,y-1,2], [x+1,y,1], [x-1,y,0]]  # /
   rights =  [[x,y-1,2], [x,y+1,3], [x-1,y,0], [x+1,y,1]]  # \
   moves = {
@@ -38,13 +34,12 @@ i = 0
 while i < 1
   tracks = input.dup.map{|c| c.dup }
   carts.each{|c| 
-    x, y, d = move(c['pos'], c['dir'], input)
+    x, y, d = move(c, input)
     tracks[y][x] = symbols[d]
-    c['pos'] = [x, y]
-    c['dir'] = d
+    c[0, 2] = [[x, y], d]
   }
 
-  puts tracks
-  puts carts
   i += 1
 end
+
+puts tracks
