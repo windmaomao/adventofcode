91f0ec6d6426d4369422b9ac7fe4834d6f831d22
Fang <windmaomao@gmail.com>
2020-01-14 15:28:38 -0500

    add day14 part2

--- 2018/rb/14.rb
+++ 2018/rb/14.rb
@@ -16,7 +16,14 @@ def genRecipes(n, goal)
 
     recipes[rcount] = sum % 10
     rcount += 1
-    found = goal && (recipes[rcount-5,5] == [5,9,4,1,4])
+    # using recipes[range] is really slow
+    found = goal && (
+      [
+        recipes[rcount-6], recipes[rcount-5],
+        recipes[rcount-4], recipes[rcount-3],
+        recipes[rcount-2], recipes[rcount-1]
+      ] == goal
+    )
 
     r1 = (r1 + 1 + recipes[r1]) % rcount
     r2 = (r2 + 1 + recipes[r2]) % rcount
@@ -35,6 +42,6 @@ rs, count = genRecipes(N, nil)
 p [N..N+9].map{|i| rs[i]}.join('')
 
 # Part 2
-G = "51589".chars.map(&:to_i)
+G = input.chars.map(&:to_i)
 rs, count = genRecipes(N, G)
 p count
