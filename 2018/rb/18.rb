9a5eaaed3997ab4f44cae3bea90b15a24b7a52ec
Fang <windmaomao@gmail.com>
2020-01-15 21:49:32 -0500

    add live rules

--- 2018/rb/18.rb
+++ 2018/rb/18.rb
@@ -14,11 +14,18 @@ x, y = [1, 1]
 sto = list.dup
 while (y <= n)
   i = y * n2 + x
-  p nbs.map{|v| list[i+v]}
-  sto[i] = "?"
+  sour = nbs.map{|v| list[i+v]}
+
+  c = list[i]
+  tc = sour.count('|')
+  lc = sour.count('#')
+  sto[i] = '|' if (c == '.') && (tc >= 3)
+  sto[i] = '#' if (c == '|') && (lc >= 3)
+  sto[i] = '.' if (c == '#') && (tc < 1 || lc < 1)
+
   x += 1
   if (x > n) then x, y = [1, y + 1] end
 end
 
-# list = sto
+list = sto
 puts list.scan(/.{#{n2}}/)
