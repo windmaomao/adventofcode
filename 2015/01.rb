286c6ce17dd1fbcffe7f0e0d1b0f27ce5e871a33
Fang <windmaomao@gmail.com>
2020-01-18 19:47:01 -0500

    add day1

--- 2015/01.rb
+++ 2015/01.rb
@@ -1,1 +1,16 @@
-p 123
+fn = open("2015/01.input")
+input = fn.each_line.to_a[0].freeze
+
+# Part 1
+p input.count("(") - input.count(")")
+
+# Part 2
+i = 0
+floor = 0
+while i < input.size
+  floor += 1 if input[i] == "("
+  floor -= 1 if input[i] == ")"
+  i += 1
+  break if floor == -1
+end
+p i
