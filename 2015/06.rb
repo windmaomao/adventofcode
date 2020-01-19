66b7ffd53b8403b4395aa8c77e41e685872ca26d
Fang <windmaomao@gmail.com>
2020-01-19 14:09:52 -0500

    add day 6 part 2

--- 2015/06.rb
+++ 2015/06.rb
@@ -5,12 +5,8 @@ input = fn.each_line.map(&:chomp).map{|l|
   [flags.index(l[6])] + l.scan(/\d+/).map(&:to_i)
 }
 
+# Part 1
 
-# input = [
-#   [1, 0, 0, 9, 9],
-#   [2, 0, 0, 9, 0],
-#   [0, 0, 9, 9, 9]
-# ]
 N = 1000
 lights = Array.new(N*N, 0)
 
@@ -27,4 +23,24 @@ input.each{|l|
 }
 
 p lights.count(1)
-# p lights
+
+# Part 2
+
+lights = Array.new(N*N, 0)
+
+input.each{|l|
+  op, l, t, r, b = l
+  (t..b).each{|y| 
+    (l..r).each{|x|
+      p = y * N + x
+      if op == 0
+        lights[p] -= 1
+        lights[p] = 0 if lights[p] < 0
+      else
+        lights[p] += op
+      end
+    }
+  }
+}
+
+p lights.inject(&:+)
