5f7b9752b01f80c3c6ab56322b9ef9c80a011d7d
Fang <windmaomao@gmail.com>
2020-01-19 09:21:23 -0500

    add day5 part 2

--- 2015/05.rb
+++ 2015/05.rb
@@ -1,6 +1,8 @@
 fn = open("2015/05.input")
 input = fn.each_line.map(&:chomp)
 
+# Part 1
+
 def found(str)
   return false if str =~ /ab|cd|pq|xy/
   return false unless str =~ /([a-z])\1/
@@ -8,3 +10,13 @@ def found(str)
 end
 
 p input.map{|l| found(l)}.count(true)
+
+# Part 2
+
+def found2(str)
+  return false unless str =~ /(\w.)\w*\1/
+  return false unless str =~ /([a-z]).\1/
+  true
+end
+
+p input.map{|l| found2(l)}.count(true)
