b101b078b0cb7d708ca7255d8ef7e214d3add395
Fang <windmaomao@gmail.com>
2020-01-19 21:28:04 -0500

    refine

--- 2015/07.rb
+++ 2015/07.rb
@@ -5,22 +5,24 @@ input = fn.each_line.map(&:chomp).map{|l|
   ins = l.scan(/[a-z]+|\d+|[A-Z]+/)
   names = l.scan(/[a-z]+/)
   name = names.pop
-  Deps[name] = [names, ins - [name]]
+  Deps[name] = [names, ins - [name], nil]
 }
 
-p Deps.size
+def eval(eqn, names, vals)
+  1
+end
 
 # Part 1
 
 found = []
-res = []
-i = 0
 while found.size < Deps.size
   selected = Deps.select{ |k, v| v[0] & found == v[0]}.keys - found  
   selected.each{|name| 
     found << name
-    res << Deps[name][1]
+    names, eqn, _ = Deps[name]
+    vals = names.map{|n| Deps[n][2]}
+    Deps[name][2] = eval(eqn, names, vals)
   }
 end
 
-p res
+p Deps
