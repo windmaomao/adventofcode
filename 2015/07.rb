c5845d10cca55dbbb308212c2cce14971ce53682
Fang <windmaomao@gmail.com>
2020-01-19 22:03:32 -0500

    get expr working

--- 2015/07.rb
+++ 2015/07.rb
@@ -1,29 +1,38 @@
 fn = open("2015/07a.input")
 
 Deps = Hash.new
-input = fn.each_line.map(&:chomp).map{|l|
+input = fn.each_line.map(&:chomp).take(3).map{|l|
   ins = l.scan(/[a-z]+|\d+|[A-Z]+/)
   names = l.scan(/[a-z]+/)
   name = names.pop
-  Deps[name] = [names, ins - [name], nil]
+  Deps[name] = [names, ins - [name]]
 }
 
-def expr(eqn, names, vals)
-  return eval(eqn[0]) if names.size < 1
-  1
+p Deps
+
+def expr(eqn, vals)
+  filled = eqn.map{|v| 
+    case v 
+    when 'AND'; '&'
+    when 'OR'; '|'
+    else
+      v.to_i.to_s == v ? v.to_i : vals[v]
+    end
+  }.join(' ')
+  eval(filled)
 end
 
 # Part 1
 
 found = []
+vals = {}
 while found.size < Deps.size
   selected = Deps.select{ |k, v| v[0] & found == v[0]}.keys - found  
   selected.each{|name| 
     found << name
-    names, eqn, _ = Deps[name]
-    vals = names.map{|n| Deps[n][2]}
-    Deps[name][2] = expr(eqn, names, vals)
+    names, eqn = Deps[name]
+    vals[name] = expr(eqn, vals)
   }
 end
 
-p Deps
+p vals
