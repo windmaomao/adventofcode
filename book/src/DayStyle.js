e981a1ca8ff1d7c8a128c50c80669fb35c7c0d02
Fang <windmaomao@gmail.com>
2020-01-20 13:21:36 -0500

    add some style to day 1

--- book/src/DayStyle.js
+++ book/src/DayStyle.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components'
 
 const DayStyle = styled.div`
+  font-family: "Source Code Pro", monospace;
+  -webkit-font-smoothing: auto;
   line-height: 1.8
+  h1, h2, h3 {
+    font-weight: bold;
+    margin: 1em 0;
+  }
+  h1 {
+    font-size: 1.5em;
+  }
+  h2 {
+    font-size: 1.3em;
+  }
+  p {
+    margin: 1em 0;
+  }
+  ul {
+    margin-left: 2em;
+    list-style-type: square;
+  }
 `
 
 export default DayStyle
