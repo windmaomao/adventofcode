26f9cb07b084986f6dfe4880b7bbb168c62734c7
Fang <windmaomao@gmail.com>
2020-01-25 18:21:24 -0500

    add a bit more blog

--- book/src/blog/BlogStyle.js
+++ book/src/blog/BlogStyle.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 
 const BlogStyle = styled.div`
+  padding: 5px;
+  font-family: "Times New Rome";
   -webkit-font-smoothing: auto;
-  line-height: 1.8
+  font-size: 16px;
+  line-height: 1.8;
   h1, h2, h3 {
     font-weight: bold;
     margin: 1em 0;
@@ -13,20 +16,20 @@ const BlogStyle = styled.div`
   h2 {
     font-size: 1.3em;
   }
-  p {
+  strong {
+    font-weight: bold;
+  }
+  p, ul {
+    font-size: 1.2em;
     margin: 1em 0;
-    width: 95%;
+  }
+  blockquote {
+    padding: 0 2em;
+    border-left: 1px solid #ddd;
   }
   ul {
     margin-left: 2em;
     list-style-type: square;
-  }
-  code {
-    border: 1px dashed gray;
-  }
-  img {
-    margin: 0.5em 0;
-    max-width: 90%;
   }
 `
 
