3ded81f682b333b86488eb2dc2beec5652bfc51d
Fang <windmaomao@gmail.com>
2020-05-01 16:14:37 -0400

    add dynamic day loading

--- 2015/01.js
+++ 2015/01.js
@@ -1,13 +1,6 @@
-import debug from 'debug'
-import { input } from './utils'
-
-const print1 = debug('day1:part1')
-const print2 = debug('day1:part2')
-const data = input('01.input', '')
-
 const d = s => s === '(' ? 1 : -1
 const sumFloor = (acc, s) => acc + d(s)
-const reachBasement = (acc, s, i) => {
+const sum2Basement = (acc, s, i) => {
   if (acc.value === -1) return acc
   return { 
     pos: i + 1,
@@ -15,10 +8,13 @@ const reachBasement = (acc, s, i) => {
   }
 }
 
-const part1 = () => data.reduce(sumFloor, 0)
-const part2 = () => data.reduce(reachBasement, { value: 0, pos: 0 }).pos
+const part1 = data => data
+  .reduce(sumFloor, 0)
 
-export default () => {
-  print1(part1())
-  print2(part2())
-}
+const part2 = data => data
+  .reduce(
+    sum2Basement, 
+    { value: 0, pos: 0 }
+  ).pos
+
+export default () => ({ part1, part2 })
--- 2015/index.js
+++ 2015/index.js
@@ -1,3 +1,17 @@
-import main from './01'
+import { input } from './utils'
+import debug from 'debug'
+// import main from './01'
 
-main()
+const day = '01'
+
+async function load() {
+  const parts = await import('./01')
+
+  const data = input(`${day}.input`, '')
+  const { part1, part2 } = parts.default()
+  const print = part => debug(`day${day}:part${part}`)
+  print(1)(part1(data))
+  print(2)(part2(data))
+}
+
+load()
