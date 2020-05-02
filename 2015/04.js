93b19f4bc06d2c14136f36f7a7ce3c28387c20dd
Fang <windmaomao@gmail.com>
2020-05-02 09:37:37 -0400

    set stage 0 to be setup stage

--- 2015/04.js
+++ 2015/04.js
@@ -1,15 +1,14 @@
 const md5 = require('md5')
 
 const base = 'bgvyzdsv'
-const zeros = n => new Array(n).fill('0').join('')
 const md5Match = n => {
-  const z = zeros(n)
+  const z = new Array(n).fill('0').join('')
   return i => md5(`${base}${i}`).slice(0, n) === z
 }
-function* genMd5(n) {
-  let i = 0, match = md5Match(n)
+function* genMd5() {
+  let i = 0, match
   while(true) {
-    if (match(i)) {
+    if (i ===0 || match(i)) {
       const rematch = yield i
       if (rematch) match = md5Match(rematch)
     }
@@ -17,8 +16,9 @@ function* genMd5(n) {
   }
 }
 
-const iter = genMd5(5)
-const part1 = data => iter.next().value
+const iter = genMd5()
+iter.next()
+const part1 = data => iter.next(5).value
 const part2 = data => iter.next(6).value
 
 export default () => ({ part1, part2 })
