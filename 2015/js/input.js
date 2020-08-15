b5d2e126f329844da5f41f4138403ab24b25572e
Fang <windmaomao@gmail.com>
2020-08-15 07:56:37 -0400

    remove exp

--- 2015/js/input.js
+++
@@ -1,9 +0,0 @@
-const input = (
-  fn, separator = '\n', toNumber = false
-) => require('fs')
-  .readFileSync(__dirname + '/../' + fn, 'utf-8')
-  .split(separator)
-  // .filter(Boolean)
-  .map(n => toNumber ? parseInt(n) : n)
-
-module.exports = input
