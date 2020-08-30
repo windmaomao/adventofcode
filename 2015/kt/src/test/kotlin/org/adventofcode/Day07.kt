331b8d711a57d366558eaf1a71081bf981f69c5a
Fang <windmaomao@gmail.com>
2020-08-29 20:02:14 -0400

    add more tests

--- 2015/kt/src/test/kotlin/org/adventofcode/Day07.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day07.kt
@@ -6,7 +6,7 @@ class Graph {
 
   fun depends(child: String, parent: String) {
     if (!tree.containsKey(parent)) {
-      tree.put(parent, "");
+      tree.put(parent, "")
     }
     tree.put(child, parent);
   }
@@ -24,5 +24,60 @@ class Graph {
   }
 }
 
+data class Equation(
+  val op: String,
+  val inputs: List<String>,
+  val output: String
+) {
+  fun eval(values: HashMap<String, Int>): Int {
+    val _i = { i: Int -> inputs[i].toInt() }
+    val _v = { i: Int ->
+      values.getOrDefault(inputs[i], 0)
+    }
+    return when (op) {
+      "AND" -> _v(0) and _v(1)
+      "OR" -> _v(0) or _v(1)
+      "LSHIFT" -> _v(0) shl _i(1)
+      "RSHIFT" -> _v(0) shr _i(1)
+      "NOT" -> {
+        var t = _v(0).inv()
+        if (t < 0) { t += 65536 }
+        t
+      }
+      else -> _i(0)
+    }
+  }
+}
+
 class Day07(name: String = "07"): Day(name) {
+  fun getEquation(s: String): Equation {
+    val parts = s.split(" -> ")
+    val op = "[A-Z]+".toRegex().find(parts[0])?.value ?: ""
+    val inputs = "[a-z]+|\\d+".toRegex()
+      .findAll(parts[0]).map{ it.value }.toList()
+    return Equation(op, inputs ,parts[1])
+  }
+
+  fun part1(list: List<String>): Int {
+    val g = Graph()
+    val equations: HashMap<String, Equation> = HashMap<String, Equation>()
+    val values: HashMap<String, Int> = HashMap<String, Int>()
+
+    list.map { getEquation(it) }.forEach { e ->
+      equations.put(e.output, e)
+      e.inputs.forEach { input ->
+        g.depends(input, e.output)
+      }
+    }
+
+    val l = g.tsort("a")
+
+    l.forEach { output ->
+      values["output"] = equations[output]?.eval(values) ?: 0
+    }
+
+    return values["a"] ?: 0
+  }
+
+
 }
--- 2015/kt/src/test/kotlin/org/adventofcode/Day07Test.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day07Test.kt
@@ -19,4 +19,36 @@ class Day07Test {
     assertEquals(listOf("c", "d", "b", "e", "a"), g.tsort("a"))
   }
 
+  @Test
+  fun day07Equation() {
+    val v: HashMap<String, Int> = HashMap<String, Int>()
+    assertEquals(123, Equation("", listOf("123"),"x").eval(v))
+    v.put("x", 123);
+    assertEquals(456, Equation("", listOf("456"),"y").eval(v))
+    v.put("y", 456);
+    assertEquals(72, Equation("AND", listOf("x", "y"),"d").eval(v))
+    v.put("d", 72);
+    assertEquals(507, Equation("OR", listOf("x", "y"),"e").eval(v))
+    v.put("e", 507);
+    assertEquals(492, Equation("LSHIFT", listOf("x", "2"),"f").eval(v))
+    v.put("f", 492);
+    assertEquals(114, Equation("RSHIFT", listOf("y", "2"),"g").eval(v))
+    v.put("g", 114);
+    assertEquals(65412, Equation("NOT", listOf("x"),"h").eval(v))
+    v.put("h", 65412);
+  }
+
+  @Test
+  fun day07Part1Example() {
+    val (op, inputs, output) = d.getEquation("x LSHIFT 2 -> f")
+    assertEquals(
+      listOf("LSHIFT", listOf("x", "2"), "f"),
+      listOf(op, inputs, output)
+    )
+  }
+
+  @Test
+  fun day07Part1() {
+    assertEquals(16076, d.part1(d.lines))
+  }
 }
