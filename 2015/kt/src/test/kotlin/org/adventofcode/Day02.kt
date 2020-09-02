dabc2135439ec625c62622b46a5de3f77ac1b69f
Fang <windmaomao@gmail.com>
2020-09-01 21:02:14 -0400

    clean day2

--- 2015/kt/src/test/kotlin/org/adventofcode/Day02.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day02.kt
@@ -1,6 +1,21 @@
 package org.adventofcode
 
+data class Box1(val l: Int, val w: Int, val h: Int) {
+  private val m = maxOf(l, w, h)
+  fun paper(): Int {
+    return (l * w + w * h + h * l) * 2 + l * w * h / m
+  }
+  fun reserved(): Int {
+    return (l + w + h - m) * 2 + l * w * h
+  }
+}
+
 class Day02(name: String): Day(name) {
+  fun getBox(s: String): Box1 {
+    val v = s.split('x').map{ it.toInt() }
+    return Box1(v[0], v[1], v[2])
+  }
+
   data class Box(val props: List<Int>) {
     fun paper(): Int {
       val l = props[0]
@@ -26,8 +41,8 @@ class Day02(name: String): Day(name) {
     }
   }
 
-  fun part1(boxes: List<List<Int>>): Int {
-    return boxes.map { box -> Box(box).paper() }.sum()
+  fun part1(list: List<String>): Int {
+    return list.map { getBox(it).paper() }.sum()
   }
 
   fun part2(boxes: List<List<Int>>): Int {
--- 2015/kt/src/test/kotlin/org/adventofcode/Day02Test.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day02Test.kt
@@ -6,11 +6,18 @@ import org.junit.Test
 class Day02Test {
   private val d = Day02("02")
 
+  @Test fun day02Box() {
+    assertEquals(58, Box1(2,3,4).paper())
+    assertEquals(43, Box1(1,1,10).paper())
+  }
+
   @Test fun day02Part1Example() {
-    val list = listOf(listOf(2, 3, 4))
-    assertEquals(58, d.part1(list))
-    val list2 = listOf(listOf(1, 1, 10))
-    assertEquals(43, d.part1(list2))
+    assertEquals(58, d.getBox("2x3x4").paper())
+    assertEquals(43, d.getBox("1x1x10").paper())
+  }
+
+  @Test fun day02Part1() {
+    assertEquals(1588178, d.part1(d.lines))
   }
 
   @Test fun day02Part2Example() {
@@ -18,10 +25,6 @@ class Day02Test {
     assertEquals(34, d.part2(list))
     val list2 = listOf(listOf(1, 1, 10))
     assertEquals(14, d.part2(list2))
-  }
-
-  @Test fun day02Part1() {
-    assertEquals(1588178, d.part1(d.getProps()))
   }
 
   @Test fun day02Part2() {
