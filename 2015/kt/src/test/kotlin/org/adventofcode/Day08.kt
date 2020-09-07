ae4572d56fb2f7b9152c7e85c702bfb581639434
Fang <windmaomao@gmail.com>
2020-09-06 20:50:14 -0400

    add day8 part2

--- 2015/kt/src/test/kotlin/org/adventofcode/Day08.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day08.kt
@@ -1,19 +1,26 @@
 package org.adventofcode
 
 class Day08() {
-  val memory = { str: String ->
-    str
+  val diff = { str: String ->
+    val real = str
       .replace(Regex("""^""""), "")
       .replace(Regex(""""$"""), "")
       .replace(Regex("""\\[\"\\]"""), " ")
       .replace(Regex("""\\x.."""), " ")
+    str.length - real.length
   }
 
-  val diff = { str: String ->
-    str.length - memory(str).length
+  val diff2 = { str: String ->
+    val real = str
+      .replace(Regex("""[\\\"]"""), "  ")
+    real.length - str.length + 2
   }
 
-  fun part1(str: List<String>): Int {
-    return str.map(diff).sum()
+  private fun part(list: List<String>, fn: (String) -> Int): Int {
+    return list.map(fn).sum()
   }
+
+  fun part1(str: List<String>) = part(str, diff)
+  fun part2(str: List<String>) = part(str, diff2)
+
 }
--- 2015/kt/src/test/kotlin/org/adventofcode/Day08Test.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day08Test.kt
@@ -8,7 +8,7 @@ class Day08Test {
   private val lines = parseFile("08")
 
   @Test
-  fun day08Real() {
+  fun day08Diff() {
     assertEquals(2, d.diff(""""""""))
     assertEquals(2, d.diff(""""abc""""))
     assertEquals(3, d.diff(""""aaa\"aaa""""))
@@ -18,8 +18,20 @@ class Day08Test {
   }
 
   @Test
-  fun day08() {
+  fun day08part1() {
     assertEquals(1342, d.part1(lines))
   }
 
+  @Test
+  fun day08Diff2() {
+    assertEquals(4, d.diff2(""""""""))
+    assertEquals(4, d.diff2(""""abc""""))
+    assertEquals(6, d.diff2(""""aaa\"aaa""""))
+    assertEquals(5, d.diff(""""\x27""""))
+  }
+
+  @Test
+  fun day08part2() {
+    assertEquals(2074, d.part2(lines))
+  }
 }
