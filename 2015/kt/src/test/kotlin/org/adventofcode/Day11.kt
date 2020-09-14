d985b211c72f84fcf146e01911c290292d79fdf8
Fang <windmaomao@gmail.com>
2020-09-14 19:32:49 -0400

    add brutal force

--- 2015/kt/src/test/kotlin/org/adventofcode/Day11.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day11.kt
@@ -1,16 +1,48 @@
 package org.adventofcode
 
 class Day11 {
-  fun genPasswordSeq(start: String): Sequence<String> {
-    val n = 27
-    val from = { c: String -> c[0].toInt() - 96 }
-    val to = { c: Int -> (96 + c).toChar().toString() }
-    var i = start.toDecimal(n, from)
+//  fun genPasswordSeq(start: String): Sequence<String> {
+//    val n = 27
+//    val from = { c: String -> c[0].toInt() - 96 }
+//    val to = { c: Int -> (96 + c).toChar().toString() }
+//    var i = start.toDecimal(n, from)
+//
+//    return sequence {
+//      while(true) {
+//        yield((++i).toBase(n, to))
+//      }
+//    }
+//  }
 
+  fun nextPassword(str: String): String {
+    var required = true
+    return str.chunked(1).reversed().map { s ->
+      if (!required) s else {
+        var c = (s[0].toInt() + 1).toChar()
+        if (c > 'z') "a" else {
+          required = false
+          c.toString()
+        }
+      }
+    }.reversed().joinToString("")
+  }
+
+  fun passwordSeq(str: String): Sequence<String> {
+    var s = str
     return sequence {
-      while(true) {
-        yield((++i).toBase(n, to))
+      while (true) {
+        s = nextPassword(s)
+        yield(s)
       }
     }
+  }
+
+  fun rightPassword(s: String): Boolean {
+    if ("i|o|l".toRegex().containsMatchIn(s)) return false
+    return "abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz".toRegex().containsMatchIn(s)
+  }
+
+  fun part1(start: String): String {
+    return passwordSeq(start).find(::rightPassword) ?: ""
   }
 }
--- 2015/kt/src/test/kotlin/org/adventofcode/Day11Test.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day11Test.kt
@@ -5,14 +5,22 @@ import org.junit.Test
 
 class Day11Test {
   private val d: Day11 = Day11()
+  private val str = "vzbxkghb"
 
   @Test
   fun day11Passwords() {
-    Assert.assertEquals(
-      listOf("ay", "az", "b`", "ba"),
-      d.genPasswordSeq("ax").take(4).toList()
-    )
+    Assert.assertEquals("ay", d.nextPassword("ax"))
+    Assert.assertEquals("ba", d.nextPassword("az"))
+  }
 
+  @Test
+  fun day11RightPassword() {
+    Assert.assertEquals(true, d.rightPassword("abbcdeffg"))
+  }
+
+  @Test
+  fun day11part1() {
+    Assert.assertEquals("xxx", d.part1(str))
   }
 
 }
