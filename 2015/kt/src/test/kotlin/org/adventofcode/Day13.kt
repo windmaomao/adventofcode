08c44f233cca15920b03ee20e1d7ab36dcdc6add
Fang <windmaomao@gmail.com>
2020-09-15 10:29:16 -0400

    add day13 part1

---
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day13.kt
@@ -0,0 +1,32 @@
+package org.adventofcode
+
+class Day13 {
+  fun extractSitting(s: String): Triple<String, String, Int> {
+    val p0 = s.split(" happiness units by sitting next to ")
+    val p1 = p0[0].split(" ")
+    val p2 = p0[1].split(".")
+    val n = s.extractNumbers()[0]
+    val gain = if (s.contains("gain")) n else -n
+    return Triple(p1[0], p2[0], gain)
+  }
+
+  fun part(list: List<String>): List<Int> {
+    val m: Map = Map()
+    list.forEach {
+      val (from, to, gain) = extractSitting(it)
+      m.addEdge(from, to, gain)
+    }
+    val happiness = { list: List<String> ->
+      val n = list.size
+      list.mapIndexed { i, p ->
+        val prev = if (i == 0) n - 1 else i - 1
+        val next = if (i == n - 1) 0 else i + 1
+        (m.edges[p]?.get(list[prev]) ?: 0)+ (m.edges[p]?.get(list[next]) ?: 0)
+      }.sum()
+    }
+    return m.getNodes().permutations().map(happiness)
+  }
+
+  fun part1(list: List<String>) = part(list).max()
+//  fun part2(list: List<String>) = part(list).max()
+}
---
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day13Test.kt
@@ -0,0 +1,53 @@
+package org.adventofcode
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Day13Test {
+  private val d: Day13 = Day13()
+  val lines = parseFile("13")
+
+  @Test
+  fun day13ExtractSitting() {
+    assertEquals(
+      Triple("Alice", "Bob", -57),
+      d.extractSitting("Alice would lose 57 happiness units by sitting next to Bob.")
+    )
+    assertEquals(
+      Triple("Alice", "Eric", 71),
+      d.extractSitting("Alice would gain 71 happiness units by sitting next to Eric.")
+    )
+  }
+
+  @Test
+  fun day13Part1Example() {
+    assertEquals(
+      330,
+      d.part1(listOf(
+        "Alice would gain 54 happiness units by sitting next to Bob.",
+        "Alice would lose 79 happiness units by sitting next to Carol.",
+        "Alice would lose 2 happiness units by sitting next to David.",
+        "Bob would gain 83 happiness units by sitting next to Alice.",
+        "Bob would lose 7 happiness units by sitting next to Carol.",
+        "Bob would lose 63 happiness units by sitting next to David.",
+        "Carol would lose 62 happiness units by sitting next to Alice.",
+        "Carol would gain 60 happiness units by sitting next to Bob.",
+        "Carol would gain 55 happiness units by sitting next to David.",
+        "David would gain 46 happiness units by sitting next to Alice.",
+        "David would lose 7 happiness units by sitting next to Bob.",
+        "David would gain 41 happiness units by sitting next to Carol."
+      ))
+    )
+  }
+
+  @Test
+  fun day13Part1() {
+    assertEquals(618, d.part1(lines))
+  }
+
+//  @Test
+//  fun day12Part2() {
+//    assertEquals(96852, d.part2(line))
+//  }
+
+}
