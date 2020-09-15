93983d6cd10d25eaf9cd2060cdf947385b4a3df5
Fang <windmaomao@gmail.com>
2020-09-15 12:05:55 -0400

    remove first zero step

--- 2015/kt/src/test/kotlin/org/adventofcode/Day14.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day14.kt
@@ -1,13 +1,16 @@
 package org.adventofcode
 
 class Day14 {
+  fun extractReindeer(s: String): Triple<Int, Int, Int> {
+    val ns = s.extractNumbers()
+    return Triple(ns[0], ns[1], ns[2])
+  }
 
   fun posSeq(speed: Int, fly: Int, rest: Int): Sequence<Int> {
     var pos = 0
     var mode = 0
     var timer = 0
     return sequence {
-      yield(0)
       while (true) {
         // reset timer
         if (timer == 0) {
@@ -23,4 +26,20 @@ class Day14 {
       }
     }
   }
+
+  fun part1(list: List<String>): Int {
+    val n = 2503
+    return list.map {
+      val (speed, fly, rest) = extractReindeer(it)
+      posSeq(speed, fly, rest).take(n).last()
+    }.max() ?: 0
+  }
+
+//  fun part2(list: List<String>): Int {
+//    val n = 2503 + 1
+//    return list.map {
+//      val (speed, fly, rest) = extractReindeer(it)
+//      posSeq(speed, fly, rest).take(n).last()
+//    }.max() ?: 0
+//  }
 }
--- 2015/kt/src/test/kotlin/org/adventofcode/Day14Test.kt
+++ 2015/kt/src/test/kotlin/org/adventofcode/Day14Test.kt
@@ -5,23 +5,36 @@ import org.junit.Test
 
 class Day14Test {
   private val d: Day14 = Day14()
-//  val lines = parseFile("13")
+  val lines = parseFile("14")
 
   @Test
-  fun day14PosSeq() {
+  fun day14ExtractDeers() {
     assertEquals(
-      listOf(0, 1, 2, 2, 2, 3, 4),
-      d.posSeq(1, 2, 2).take(7).toList()
-    )
-    assertEquals(
-      listOf(0, 1, 2, 3, 3, 4, 5),
-      d.posSeq(1, 3, 1).take(7).toList()
-    )
-    assertEquals(
-      listOf(0, 2, 2, 2, 4, 4, 4),
-      d.posSeq(2, 1, 2).take(7).toList()
+      Triple(14, 10, 127),
+      d.extractReindeer("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.")
     )
   }
 
+  @Test
+  fun day14PosSeq() {
+    val n = 6
+    assertEquals(
+      listOf(1, 2, 2, 2, 3, 4),
+      d.posSeq(1, 2, 2).take(n).toList()
+    )
+    assertEquals(
+      listOf(1, 2, 3, 3, 4, 5),
+      d.posSeq(1, 3, 1).take(n).toList()
+    )
+    assertEquals(
+      listOf(2, 2, 2, 4, 4, 4),
+      d.posSeq(2, 1, 2).take(n).toList()
+    )
+  }
+
+  @Test
+  fun day14Part1() {
+    assertEquals(2655, d.part1(lines))
+  }
 
 }
