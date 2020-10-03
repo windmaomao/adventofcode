c605069b1e89987450252416b2341b21c87b1286
Fang <windmaomao@gmail.com>
2020-10-03 19:07:07 -0400

    d06 refine

--- 2015/js/day06.js
+++ 2015/js/day06.js
@@ -8,7 +8,6 @@ const extractOp = str => ({
 })
 
 const N = 1000
-const N2 = N * N
 
 const toggleLights = toggleFn => (lights, op) => {
   const [l, t, r, b] = op.box
@@ -24,33 +23,12 @@ const toggleLights = toggleFn => (lights, op) => {
 
 const toggle = (v, type) => ((type > 1) ? 1 - v : type)
 const part1 = ops => ops
-  .reduce(toggleLights(toggle), Array.new(N2, 0))
+  .reduce(toggleLights(toggle), Array.new(N*N, 0))
   .count(1)
 
-export { extractOp, part1 }
+const bright = (v, type) => ((type == 0) ? Math.max(0, v - 1) : v + type)
+const part2 = ops => ops
+  .reduce(toggleLights(bright), Array.new(N*N, 0))
+  .sum()
 
-// const initLights = () => Array.new(N * N, 0)
-//
-// const toggle = (v, op) => ((op > 1) ? 1 - v : op)
-// const bright = (v, op) => ((op === 0) ? Math.max(0, v - 1) : v + op)
-// const toggleLights = toggleFn => (lights, v) => {
-//   const [op, l, t, r, b] = v
-//   range(t, b + 1).forEach(y => {
-//     const row = y * N
-//     range(l, r + 1).forEach(x => {
-//       const p = row + x
-//       lights[p] = toggleFn(lights[p], op)
-//     })
-//   })
-//   return lights
-// }
-//
-// const prepare = data => data.map(processData)
-// const part1 = data => data
-//   .reduce(toggleLights(toggle), initLights())
-//   .filter(Math.sign).length
-// const part2 = data => data
-//   .reduce(toggleLights(bright), initLights())
-//   .sum()
-//
-// export default () => ({ prepare, part1, part2 })
+export { extractOp, part1, part2 }
--- 2015/js/day06.test.js
+++ 2015/js/day06.test.js
@@ -1,13 +1,16 @@
-import { extractOp, part1 } from './day06'
+import { extractOp, part1, part2 } from './day06'
 import read from './read'
 
 describe('2015 Day 06', () => {
 
   const ops = read('06').map(extractOp)
-// console.log(ops)
+
   it('part1', () => {
     expect(part1(ops)).toEqual(543903)
   })
 
+  it('part2', () => {
+    expect(part2(ops)).toEqual(14687245)
+  })
 
 })
