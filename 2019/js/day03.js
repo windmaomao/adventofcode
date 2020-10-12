3b2dbaf3a71ab6f18c2bbffbe494d61df082a421
Fang <windmaomao@gmail.com>
2020-10-12 11:02:34 -0400

    2019 d3 js

--- 2019/js/day03.js
+++ 2019/js/day03.js
@@ -1,30 +1,27 @@
 import '../../utils/js/array'
 import '../../utils/js/math'
 
+const extractIns = str => str
+  .split(',')
+  .flatMap(s => {
+    const n = parseInt(s.slice(1))
+    return Array.new(n, s[0])
+  })
+
 const dirs = {
   'U': [ 0, 1], 'D': [0, -1],
   'L': [-1, 0], 'R': [1,  0]
 }
 
-const posKey = p => `${p[0]}x${p[1]}`
-const dist = p => p.sum(Math.abs)
+const part1 = strs => strs
+  .map(extractIns)
+  .flatMap(ins => ins
+    .map(i => dirs[i])
+    .scan(Math.plusN, [0,0])
+    .slice(1)
+  )
+  .duplicate(p => `${p[0]}x${p[1]}`)
+  .map(p => p[0].sum(Math.abs))
+  .min()
 
-const duplicates = list => {
-  const m = {}
-  list.forEach(p => {
-    const k = posKey(p)
-    m[k] = m[k] || []
-    m[k].push(p)
-  })
-  return Object
-    .keys(m)
-    .filter(k => m[k].length > 1)
-}
-
-// const part1 = dirs => dirs
-//   .scan(Math.plusN, [0,0])
-//   .then(findDuplicate)
-//   .map(d => dist(d[0]))
-//   .min()
-
-export { dirs, part1, part2 }
+export { extractIns, part1 }
---
+++ 2019/js/day03.test.js
@@ -0,0 +1,34 @@
+import { extractIns, part1 } from './day03'
+import read from './read'
+
+describe('2019 Day 03', () => {
+
+  const list = read('3')
+
+  const example0 = ['R8,U5,L5,D3', 'U7,R6,D4,L4']
+  const example1 = [
+    'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+    'U62,R66,U55,R34,D71,R55,D58,R83'
+  ]
+  const example2 = [
+    'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+    'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+  ]
+
+  it('part1 extractIns', () => {
+    expect(extractIns('D1')).toEqual(['D'])
+    expect(extractIns('D3')).toEqual(['D', 'D', 'D'])
+    expect(extractIns('A1,B2')).toEqual(['A', 'B', 'B'])
+  })
+
+  it('part1 example', () => {
+    expect(part1(example0)).toEqual(6)
+    expect(part1(example1)).toEqual(157)
+    expect(part1(example2)).toEqual(135)
+  })
+
+  it('part1', () => {
+    expect(part1(list)).toEqual(266)
+  })
+
+})
