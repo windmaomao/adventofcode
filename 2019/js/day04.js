50b06341b5ced90ae973cad13236364ea415b633
Fang <windmaomao@gmail.com>
2020-10-12 12:48:32 -0400

    2019 d4 js

--- 2019/js/day04.js
+++ 2019/js/day04.js
@@ -1,12 +1,16 @@
+import '../../utils/js/array'
+
 const numValid = num => {
   const s = `${num}`
   if (!s.match(/(.)\1/g)) return false
-
-  for (let i = 0; i < s.length - 1; i++) {
-    if (s[i] > s[i+1]) return false
-  }
-
-  return true
+  return !s.split('').windowed(2).some(a => a[0] > a[1])
 }
 
-export { numValid }
+const part1 = r => Array
+  .range(r[0], r[1])
+  .map(numValid)
+  .count(true)
+
+// const part2 = 
+
+export { numValid, part1 }
--- 2019/js/day04.test.js
+++ 2019/js/day04.test.js
@@ -1,14 +1,18 @@
-import { numValid } from './day04'
-// import read from './read'
+import { numValid, part1 } from './day04'
+import read from './read'
 
 describe('2019 Day 04', () => {
 
-  // const list = read('')
+  const range = read('4')[0].split('-').map(Number)
 
   it('part1 numValid', () => {
     expect(numValid(111111)).toEqual(true)
     expect(numValid(223450)).toEqual(false)
     expect(numValid(123789)).toEqual(false)
+  })
+
+  it('part1', () => {
+    expect(part1(range)).toEqual(495)
   })
 
 })
