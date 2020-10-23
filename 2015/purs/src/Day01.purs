435ee21d2e3da1916383905899b69e5e1d41f2bd
Fang <windmaomao@gmail.com>
2020-10-23 16:46:50 -0400

    get day1 for purs

--- 2015/purs/src/Day01.purs
+++ 2015/purs/src/Day01.purs
@@ -3,8 +3,20 @@ module Day01 where
 import Prelude
 import Data.Foldable
 import Data.String.CodeUnits
+import Data.Array (takeWhile, scanl, length)
 
 floor :: Char -> Int
-floor x = 3
+floor '(' = 1
+floor c = -1
 
-part1 = sum <<< map floor <<< toCharArray
+part1 =
+  sum <<<
+  map floor <<<
+  toCharArray
+
+part2 =
+  length <<<
+  takeWhile (_ > 0) <<<
+  scanl (+) 0 <<<
+  map floor <<<
+  toCharArray
--- 2015/purs/test/Main.purs
+++ 2015/purs/test/Main.purs
@@ -4,9 +4,10 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Class.Console (log, logShow)
-import Day01 (floor, part1)
+import Day01 (floor, part1, part2)
 
 main :: Effect Unit
 main = do
   log "🍝"
-  (logShow <<< part1) "^^vv"
+  (logShow <<< part1) "(()))"
+  (logShow <<< part2) "(()))"
