87c4ab969d7dd8f1be2660ede47e512da6253515
Fang <windmaomao@gmail.com>
2020-12-08 17:15:54 -0500

    add day 4part 2 hs

--- 2020/hs/day04.hs
+++ 2020/hs/day04.hs
@@ -12,16 +12,55 @@ pps lines = keyvalue <$> group <$> groups
 		keyvalue g = fmap (splitOn ":") g
 		
 requiredKeys = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+eclOptions = ["amb","blu","brn","gry","grn","hzl","oth"]
 
 part1 :: [Passport] -> Int
 part1 pps = length $ filter valid pps
 	where
 		keys g = fmap head g
-		requires = ["byr"]
 		haveKeys g = (keys g) `intersect` requiredKeys
 		valid g = length (haveKeys g) >= 7
-		
+
+within :: Int -> Int -> String -> Bool
+within l h s= n >= l && n <= h
+	where n = read s :: Int
+
+lastN :: Int -> String -> String
+lastN n xs = drop (length xs - n) xs
+
+takeN :: Int -> String -> String
+takeN n xs = take (length xs - n) xs
+
+fieldValid :: [String] -> Bool
+fieldValid ["byr", v] = within 1920 2002 v
+fieldValid ["iyr", v] = within 2010 2020 v
+fieldValid ["eyr", v] = within 2020 2030 v
+fieldValid ["hgt", v]
+	| unit == "cm" 	= within 150 193 num
+	| unit == "in"  = within 59 76 num
+	| otherwise = False
+	where
+		unit = lastN 2 v
+		num = takeN 2 v
+fieldValid ["hcl", '#':xs] 	= length xs == 6
+fieldValid ["hcl", _] 			= False
+fieldValid ["ecl", v] = v `elem` eclOptions
+fieldValid ["pid", v] = length v == 9
+fieldValid ["cid", _] = True
+fieldValid [_, _] = False
+
+part2 :: [Passport] -> Int
+part2 pps = length $ filter joined pps
+	where
+		joined g = valid g && valid2 g
+		valid2 g = all id $ fieldValid <$> g
+		keys g = fmap head g
+		haveKeys g = (keys g) `intersect` requiredKeys
+		valid g = length (haveKeys g) >= 7
+
 main :: IO ()
 main = do
 	fn <- lines <$> readFile "../res/04.input"
-	print $ part1 $ pps fn
+	let s = pps fn
+	print $ part1 s
+	print $ part2 s
--- README.md
+++ README.md
@@ -1,26 +1,11 @@
-# Adevent of Code
-
-Written in `Javascript`, `Ruby`, `Kotlin` and `Haskell`.
-
-I started with `Javascript`, half is written with it.
-
-During the 2019 competition, I found `Ruby` very easy to use, therefore spent some time with it.
-
-Over the time, I looked for type-safe, which led me to `Kotlin`. I started to code it in 2015 folder.
-
-After a year also, I get interested in `Haskell`, therefore I started to blend some in 2020 folder.
-
+# Advent of Code with Javascript
+I started `Advent of Code` with `Javascript` at 2019. During the competition, I found `Ruby` very easy to use, so experiment with it that year. At 2020, I looked for a type-safe language, which led me to `Kotlin` in 2015 folder. Later I got interested at `Haskell`, and blended some in 2020 folder.
 ## Usage for later `Javascript` and `Haskell`
-
 use CodeRunner editor, basically just run it.
-
 ## Usage for `Kotlin`
-
 use IntelliJ editor, run test folder.
-
 ## Usage for early `Javascript` and `Ruby`
-
-```
+```bash
   npm install
   npm run 2015
   npm start
