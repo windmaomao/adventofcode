8a6a8f73cd72da3332825030cce2a5b9dfe12a9f
Fang <windmaomao@gmail.com>
2020-12-12 00:27:08 -0500

    day12 js

---
+++ 2020/js/day12.js
@@ -0,0 +1,50 @@
+const read = require('./read.js')
+const arr = read('12').map(v => [v[0], v.slice(1)])
+
+const DIRS = {
+  E: [1, 0], N: [0, 1], S: [0, -1], W: [-1, 0]
+}
+const ANGLES = ['E', 'S', 'W', 'N']
+
+const rotate = (curr, degree) => {
+  const n = parseInt(degree / 90)
+  return ANGLES[(ANGLES.indexOf(curr) + n + 4) % 4]
+}
+
+const part1 = instructions => {
+  const res = instructions.reduce((acc, ins) => {
+    const [id, dt] = ins
+    const pos = acc.pos
+    let dir = acc.dir, move = true
+    switch(id) {
+      case 'F':
+        break
+      case 'E':
+      case 'W':
+      case 'N':
+      case 'S':
+        dir = id
+        break
+      case 'R':
+        dir = rotate(dir, dt)
+        move = false
+        break
+      case 'L':
+        dir = rotate(dir, -dt)
+        move = false
+        break
+    }
+    if (move) {
+      acc.pos[0] = pos[0] + DIRS[dir][0]*dt
+      acc.pos[1] = pos[1] + DIRS[dir][1]*dt
+    } else {
+      acc.dir = dir
+    }
+    return acc
+  }, { dir: 'E', pos: [0, 0] })
+  
+  return Math.abs(res.pos[0]) + Math.abs(res.pos[1])
+}
+
+//console.log(arr)
+console.log(part1(arr))
