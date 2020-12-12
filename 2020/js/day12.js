5f8d1f5d7b314cec9102762733c6fb3932593a0c
Fang <windmaomao@gmail.com>
2020-12-12 00:51:33 -0500

    add day12 js part2

--- 2020/js/day12.js
+++ 2020/js/day12.js
@@ -46,5 +46,41 @@ const part1 = instructions => {
   return Math.abs(res.pos[0]) + Math.abs(res.pos[1])
 }
 
+const rotateWp = (wp, degree) => {
+  const [x,y] = wp
+  const tmp = [[x, y], [y, -x], [-x, -y], [-y, x]]
+  const n = parseInt(degree / 90)
+  return tmp[(n + 4) % 4]
+}
+
+const part2 = instructions => {
+  const res = instructions.reduce((acc, ins) => {
+    const [id, dt] = ins
+
+    switch(id) {
+      case 'F':
+        acc.pos[0] += acc.wp[0]*dt
+        acc.pos[1] += acc.wp[1]*dt
+        break
+      case 'E':
+      case 'W':
+      case 'N':
+      case 'S':
+        acc.wp[0] += DIRS[id][0]*dt
+        acc.wp[1] += DIRS[id][1]*dt
+        break
+      case 'R':
+        acc.wp = rotateWp(acc.wp, dt)
+        break
+      case 'L':
+        acc.wp = rotateWp(acc.wp, -dt)
+    }
+      
+    return acc
+  }, { wp: [10, 1], pos: [0, 0] })
+  
+  return Math.abs(res.pos[0]) + Math.abs(res.pos[1])
+}
 //console.log(arr)
 console.log(part1(arr))
+console.log(part2(arr))
