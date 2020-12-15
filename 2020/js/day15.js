5550189f03986c8b06421172d7cbb7327bc76464
Fang <windmaomao@gmail.com>
2020-12-15 10:39:39 -0500

    experiement vector arr

--- 2020/js/day15.js
+++ 2020/js/day15.js
@@ -1,42 +1,61 @@
 const read = require('./read.js')
 const lines = read('15')[0].split(',').map(v => parseInt(v))
 
-//0,3,6,0,3,3,1,0,4,0,
+const cache = () => {
+  let i = 0, p, q
+  const unshift = n => { i++; q = p; p = n; }
+  const diff = () => (i < 2 ? 0 : p - q)
+  return { unshift, diff }
+}
 
-const part = (raw, max) => {
-  const arr = raw.slice()
+const cacheV = max => {
+  const a = new Array(max).fill(0)
+    .map(v => new Array(3).fill(0))
+    
+  return index => {
+    const s = index * 3
+    const unshift = n => { a[s+2]++; a[s+1] = a[s]; a[s] = n }
+    const diff = () => (a[s+2] < 2 ? 0 : a[s] - a[s+1])
+    return { unshift, diff }
+  }
+}
+
+const part = (arr, max) => {
   const mem = {}
   
   let i = 0, n
-  while ((p = arr.shift()) != undefined) {
-    if (!mem[p]) mem[p] = []
-    mem[p].unshift(i)
-    n = p
+  while (i < arr.length) {
+    n = arr[i]
+    if (!mem[n]) mem[n] = cache()
+    mem[n].unshift(i)
     i++
   }
   
   while (i < max) { 
-    const cache = mem[n]
-    if (cache.length == 1) {
-      n = 0
-    } else {
-      n = cache[0] - cache[1]
-    }
-    
-    if (!mem[n]) mem[n] = []
+    n = mem[n].diff()
+    if (!mem[n]) mem[n] = cache()
     mem[n].unshift(i)
-    if (mem[n].length > 2) {
-      mem[n] = [mem[n][0], mem[n][1]]
-    }
-    
     i++
   }
-  console.log(mem)
+//  console.log('m', Object.keys(mem).pop())
   return n
 }
 
 const part1 = () => part(lines, 2020)
-const part2 = () => part(lines, 1000)
+const part2 = () => part(lines, 10000000)
 
 console.log(part1())
-console.log(part2())
+//console.time('part2')
+//console.log(part2())
+//console.timeEnd('part2')
+
+//0,3,6,0,3,3,1,0,4,0,
+//const a = cache()
+//a.unshift(0); console.log(a.diff())
+//a.unshift(3); console.log(a.diff())
+//a.unshift(7); console.log(a.diff())
+
+//  10k -   19ms
+// 100k -   93ms
+//   1m -    6s
+//  10m -   87s
