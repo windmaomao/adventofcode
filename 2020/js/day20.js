f1f15db3514a14e1632413aa51093c890f117cdf
Fang Jin <windmaomao@gmail.com>
2020-12-20 15:38:27 -0500

    prepare day 20 input

--- 2020/js/day20.js
+++ 2020/js/day20.js
@@ -1,5 +1,12 @@
+const getTiles = () => lines.map(l => {
+	const [t, ...res] = l.split('\n')
+	const id = parseInt([...t.match(/\d+/)][0])
+	return { id, res }
+})
+
 const read = require('./read.js')
 const run = require('./run.js')
+const log = require('./log.js')
 
-const lines = read('20')
-console.log(lines)
+const lines = read('20a', '\n\n')
+run(getTiles)
---
+++ 2020/js/log.js
@@ -0,0 +1,22 @@
+const util = require('util')
+
+const settings = {
+//	showHidden: false, 
+	depth: null,
+	colors: true,
+//	maxArrayLength: Infinity,
+//	maxStringLength: Infinity,
+	breakLength: Infinity,
+//	compact: false
+}
+
+util.inspect.defaultOptions = {
+	...util.inspect.defaultOptions,
+	...settings
+}
+
+const log = (s, options = {}) => { 
+	console.log(util.inspect(s, options))
+}
+
+module.exports = log
