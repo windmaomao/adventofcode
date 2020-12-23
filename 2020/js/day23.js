8245322081ba4379ebd4a3592626d1232b8964d5
Fang Jin <windmaomao@gmail.com>
2020-12-23 14:31:07 -0500

    draft day23

--- 2020/js/day23.js
+++ 2020/js/day23.js
@@ -4,28 +4,46 @@ function list(items) {
     curr.next = { value: n }
     curr = curr.next
   })
-  curr.next= root.next
+  const head = root.next
+  curr.next= head
 
   const arr = () => {
-    let c = root.next
-    const res = []
-    while (c.next != root.next) { 
+    const res = [head.value]
+    let c = head.next
+    while (c != head) { 
       res.push(c.value)
-      c = c.next 
+      c = c.next
     }
-    return res
+    return res.join('')
   }
 
   return { root, arr }
 }
 
+const move = curr => {
+  let pickup = curr.next.next.next
+  let dest = pickup.next
+  pickup.next = dest.next
+  dest.next = curr.next
+  curr.next = dest
+  return dest
+}
+
 const part1 = () => {
   const nums = "389125467".split('')
+  const l = list(nums)
+  let i = 1, curr = l.root.next
+  while (i <= 3) {
+    console.log(i, l.arr())
+    curr = move(curr)
+    i++
+  }
   
-  return list(nums).arr()
+  return l.arr()
 }
 
 const read = require('./read.js')
 const num = read('23')
 const run = require('./run')
+const log = require('./log')
 run(part1, num)
