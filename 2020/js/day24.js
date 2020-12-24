65e0131f962b9c471c5d9cc90e5ad14654e2ef71
Fang Jin <windmaomao@gmail.com>
2020-12-24 13:19:27 -0500

    draft day24 part2

--- 2020/js/day24.js
+++ 2020/js/day24.js
@@ -18,9 +18,10 @@ const getDirs = lines => {
   return res
 }
 
-const part1 = () => {
+const _k = p => p.join(',')
+
+const getGrid = () => {
   const m = {}
-  const _k = p => p.join(',')
 
   lines.map(line => {
     return getDirs(line).reduce((acc, dir) => {
@@ -28,20 +29,60 @@ const part1 = () => {
       return acc.map((v, i) => v + rel[i])
     }, [0, 0])
   }).forEach(p => {
-    console.log(p)
     const k = _k(p)
     if (m[k] == undefined) m[k] = false
     m[k] = !m[k]
   })
 
-  return Object.values(m).filter(v => v).length
-} 
+  return m
+}
+
+const gameOfLife = m => {
+  let i = 0
+  const rels = Object.values(dirs)
+  let g = {...m}, g2
+  while (i < 2) {
+    const neighbours = {}
+
+    const blacks = Object.keys(g).filter(k => g[k])
+    console.log(i, blacks.length)
+    blacks.forEach(b => {
+      const p = b.split(',').map(v => parseInt(v))
+      rels.forEach(rel => {
+        const np = rel.map((r, j) => r + p[j])
+        neighbours[_k(np)] = true
+      })
+    })
+
+    g2 = {}
+    blacks.forEach(b => { g2[b] = true })
+    Object.keys(neighbours).forEach(b => {
+      const p = b.split(',').map(v => parseInt(v))
+      const bn = rels.map(rel => {
+        const np = rel.map((r, j) => r + p[j])
+        return g[_k(np)]
+      }).filter(v => v).length
+      
+      if (g[b]) {
+        if (bn == 0 || bn > 2) g2[b] = false
+      } else {
+        if (bn == 2) g2[b] = true
+      }
+    })
+
+    i++
+    g = g2
+  }
+}
+
+const part1 = m => Object.values(m).filter(v => v).length
 
 const read = require('./read.js')
-const lines = read('24')
+const lines = read('24a')
 const run = require('./run')
-run(part1)      
-
+const grid = getGrid()
+run(part1, grid)
+run(gameOfLife, grid)
 
 // (0,0) (0,1) (0,2) (0,3) (0,4)
 //    (1,0) (1,1) (1,2) (1,3) (1,4)
