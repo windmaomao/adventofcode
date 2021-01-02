8bc0638fa97fd2316d32ebd40ed6b2dc2d5d4e04
Fang Jin <windmaomao@gmail.com>
2021-01-02 13:48:57 -0500

    add 2018 day2

--- js/2018-02.js
+++ js/2018-02.js
@@ -1,14 +1,19 @@
 require('./utils/index')
-const lines = read('2018', '02a')
+const lines = read('2018', '02b')
+
+const getStats = str => {
+	const stats = Array.new(26, 0)
+	Array.new(str.length).map(i => {
+		const j = str.charCodeAt(i) - 97
+		stats[j]++
+	})
+	return stats
+}
 
 const part1 = strs => {
 	let c = [0, 0]
 	strs.forEach(str => {
-		const stats = str.split('').reduce((acc, letter) => {
-			acc[letter] = acc[letter] ? acc[letter] + 1 : 1
-			return acc 
-		}, {})
-		const vs = Object.values(stats);
+		const vs = getStats(str);
 		[2,3].map((m, i) => {
 			if (vs.filter(v => v == m).length) c[i]++
 		})
@@ -16,4 +21,25 @@ const part1 = strs => {
 	return c.multiply()
 }
 
+const part2 = strs => {
+	const allStats = strs.map(getStats)
+	log(allStats, { compact: true })
+	const n = strs.length
+	const res = Array.new(2, allStats).runN(
+		(acc, [vi, vj], [i, j]) => {
+			if (i >= j) return null
+			const diff = vi.map((v, k) => Math.abs(v - vj[k])).sum()
+			return (diff <= 2) ? [vi, vj] : null
+		}, null,
+		acc => acc
+	)
+	const [[vi, vj], [i, j]] = res
+	const str = strs[i]
+	return Array.new(str.length).filter(k => {
+		const l = str.charCodeAt(k) - 97
+		return vi[l] == vj[l]
+	}).map(k => str[k]).join('')
+}
+
 run(part1, lines)
+run(part2, lines)
--- js/utils/index.js
+++ js/utils/index.js
@@ -1,5 +1,6 @@
 global.read = require('./common/read')
 global.run = require('./common/run')
+global.log = require('./common/log')
 
 require('./number/number')
 require('./array/array')
