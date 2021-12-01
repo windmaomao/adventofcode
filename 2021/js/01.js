c057f185786fc2c659003c54ea127119ef3f135a
Fang Jin <windmaomao@gmail.com>
2021-12-01 08:05:17 -0500

    refactor day 1

--- 2021/js/01.js
+++ 2021/js/01.js
@@ -1,22 +1,26 @@
-const part = (ns) => {
-  let prev = null
-  return ns.reduce((acc, v) => {
-    if (prev && v > prev) acc++
-    prev = v
+const bundle = require('./bundle')
+const nums = bundle.read('01a', '\n', true)
+
+const part1 = ns => {
+  return ns.reduce((acc, v, i) => {
+    if (i > 0) { 
+      if (v > ns[i - 1]) acc++
+    }
     return acc
   }, 0)
 }
 
+bundle.run(part1, nums)
+
 const part2 = (ns) => {
-  const n = ns.length
-  const res = []
-  for (let i = 0; i < n - 2; i++) {
-    res.push(ns[i] + ns[i+1] + ns[i+2])
-  }
-  return part(res)
+  const res = ns.map((v, i) => {
+    if (i < ns.length - 2) {
+      return ns[i] + ns[i+1] + ns[i+2]
+    } else {
+      return -Infinity
+    }
+  })
+  return part1(res)
 }
 
-const bundle = require('./bundle')
-const nums = bundle.read('01a', '\n', true)
-bundle.run(part, nums)
 bundle.run(part2, nums)
