fa16cffdce735f9a8fb7cd0134448e23440c5bc0
Fang Jin <windmaomao@gmail.com>
2022-01-02 15:41:25 -0500

    refine world interpretation

--- problem/recursion/world_interpretation.rs
+++ problem/recursion/world_interpretation.rs
@@ -23,11 +23,13 @@ impl Maze {
     self.mat[p.0 as usize][p.1 as usize]
   }
   
-  fn search(&self, p: Pos) -> Vec<char> {
+  fn search(
+    &self, p: &Pos, keys: &Vec<char>
+  ) -> Vec<char> {
     let dirs: [(i8,i8);4] = [(-1,0), (0,1), (1,0), (0,-1)];
     let mut marked: HashMap<Pos, bool> = 
       HashMap::new();
-    let mut queue = vec![(p, 0)];
+    let mut queue = vec![(p.clone(), 0)];
     let mut dest = vec![];
     let mut i = 0;
     
@@ -35,18 +37,29 @@ impl Maze {
       if i == queue.len() { break; }
       let (p, dist) = queue[i];
       i = i + 1;
-      marked.insert(p, true);
+      marked.insert(p.clone(), true);
       
       let c = self.char(&p);
-      println!("{:?}:{}", p, c);
+      let key_taken = keys.iter()
+        .find(|&&k| k == c) != None;
+      println!("{:?}:{}, {}", p, c, key_taken);
       
-      if c.is_lowercase() { 
+      if c.is_lowercase() && !key_taken { 
         dest.push(c);
       } else {
         for d in dirs.iter() {
           let q = Pos(d.0+p.0, d.1+p.1);
           if marked.get(&q) != None { continue }
-          if self.char(&q) == '#' { continue }
+          
+          let qc = self.char(&q);
+          if qc == '#' { continue }
+          
+          if qc.is_uppercase() {
+            let door_key = qc.to_ascii_lowercase();
+            let key_found = keys.iter()
+              .find(|&&k| k == door_key) != None;
+            if !key_found { continue }
+          }
           queue.push((q, dist+1));
         }
       }
@@ -64,6 +77,6 @@ fn main() {
     
   println!("{}, {:?}", 
     m.char(&Pos(0,1)), 
-    m.search(Pos(1,5))
+    m.search(&Pos(1,5), &vec![])
   )
 }
