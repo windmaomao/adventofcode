764be30cc5d70b4a6831f49665537c9a363deebf
Fang Jin <windmaomao@gmail.com>
2022-01-20 13:04:29 -0500

    add fib algo

--- runner/src/sum.test.ts
+++ runner/src/sum.test.ts
@@ -24,7 +24,7 @@ class SumAlgo extends Algo {
     this.#res = this.#arr.reduce((acc, v) => acc + v)
   }
 
-  report(): string {
+  print(): string {
     return `${this.#res}`
   }
 
@@ -32,9 +32,17 @@ class SumAlgo extends Algo {
     super.run(inputs)
     this.parse(inputs)
     this.execute()
-    return this.report()
+    return this.print()
   }
 
+  *generate() {
+    this.#res = 0
+    yield this.print()
+    for (let v of this.#arr) {
+      this.#res += v
+      yield this.print()
+    }
+  }
 }
 
 const sum = (line: string): string => {
@@ -42,31 +50,50 @@ const sum = (line: string): string => {
   return al.run([line])
 }
 
-describe('Sum Algo', () => {
+const debug = (line: string): string => {
+  const al = new SumAlgo()
+  al.parse([line])
+  const res = []
+  for (let o of al.generate()) {
+    res.push(o)
+  }
+  return res.join('\n')
+}
 
-  it('should throw when empty', () => {
-    const al = new SumAlgo()
-    const fn = () => { al.run([]) }
-    expect(fn).toThrow('empty')
+describe('Sum Algo', () => {
+  describe('run', () => {
+    it('should throw when empty', () => {
+      const al = new SumAlgo()
+      const fn = () => { al.run([]) }
+      expect(fn).toThrow('empty')
+    })
+    
+    it('should add one number', () => {
+      expect(sum('1')).toBe('1')
+    })
+  
+    it('should add two numbers', () => {
+      expect(sum('1,2')).toBe('3')
+    })
+  
+    it('should take negative numbers', () => {
+      expect(sum('1,-2')).toBe('-1')
+    })
+  
+    it('should take float numbers', () => {
+      expect(sum('1.0,2')).toBe('3')
+    })
   })
   
-  it('should add one number', () => {
-    expect(sum('1')).toBe('1')
+  describe('debug', () => {
+    it('should add one number', () => {
+      expect(debug('1')).toBe('0\n1')
+    })
+
+    it('should add two numbers', () => {
+      expect(debug('1,2')).toBe('0\n1\n3')
+    })
+  
   })
-
-  it('should add two numbers', () => {
-    expect(sum('1,2')).toBe('3')
-  })
-
-  it('should take negative numbers', () => {
-    expect(sum('1,-2')).toBe('-1')
-  })
-
-  it('should take float numbers', () => {
-    expect(sum('1.0,2')).toBe('3')
-  })
-
-
-
 
 })
