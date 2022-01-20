205411e2afd7e068cdc5d918109bcc7689a20e92
Fang Jin <windmaomao@gmail.com>
2022-01-20 14:03:35 -0500

    create a base algo class

--- runner/src/sum.test.ts
+++ runner/src/sum.test.ts
@@ -1,99 +1,65 @@
 class Algo {
-  run(inputs: string[]): string {
-    if (inputs.length == 0) {
-      throw 'empty'
+  delimiter = /[,]/
+  parse(inputs: string[]): void {}
+  *generate(): any {}
+  debug(inputs: string[]): string[] {
+    this.parse(inputs)
+    const res = []
+    for (let o of this.generate()) {
+      res.push(o)
     }
-    return ''
+    return res
   }
 }
-
-let delimiter = /[,]/
-let parse = Number
 
 class SumAlgo extends Algo {
   #arr: number[] = []
-  #res: number
 
   parse(inputs: string[]) {
-    this.#arr = inputs[0]
-      .split(delimiter)
-      .map(s => parse(s))
-   }
-
-  execute() {
-    this.#res = this.#arr.reduce((acc, v) => acc + v)
-  }
-
-  print(): string {
-    return `${this.#res}`
-  }
-
-  run(inputs: string[]): string {
-    super.run(inputs)
-    this.parse(inputs)
-    this.execute()
-    return this.print()
+    const line = inputs[0]
+    if (!line) {
+      this.#arr = []
+    } else {
+      this.#arr = line
+        .split(this.delimiter)
+        .map(s => Number(s))
+    }
   }
 
   *generate() {
-    this.#res = 0
-    yield this.print()
-    for (let v of this.#arr) {
-      this.#res += v
-      yield this.print()
+    let res = 0, v
+    yield 'set out 0'
+    const arr = this.#arr
+    for (let i = 0; i < arr.length; i++) {
+      v = arr[i]
+      yield `get in.${i} ${v}`
+      res += v
+      yield `set out ${res}`
     }
   }
 }
 
-const sum = (line: string): string => {
-  const al = new SumAlgo()
-  return al.run([line])
-}
-
 const debug = (line: string): string => {
-  const al = new SumAlgo()
-  al.parse([line])
-  const res = []
-  for (let o of al.generate()) {
-    res.push(o)
-  }
-  return res.join('\n')
+  return (new SumAlgo()).debug([line]).join('\n')
 }
 
 describe('Sum Algo', () => {
-  describe('run', () => {
-    it('should throw when empty', () => {
-      const al = new SumAlgo()
-      const fn = () => { al.run([]) }
-      expect(fn).toThrow('empty')
-    })
-    
-    it('should add one number', () => {
-      expect(sum('1')).toBe('1')
-    })
-  
-    it('should add two numbers', () => {
-      expect(sum('1,2')).toBe('3')
-    })
-  
-    it('should take negative numbers', () => {
-      expect(sum('1,-2')).toBe('-1')
-    })
-  
-    it('should take float numbers', () => {
-      expect(sum('1.0,2')).toBe('3')
-    })
+  it('should add no number', () => {
+    expect(debug('')).toBe(`set out 0`)
   })
-  
-  describe('debug', () => {
-    it('should add one number', () => {
-      expect(debug('1')).toBe('0\n1')
-    })
 
-    it('should add two numbers', () => {
-      expect(debug('1,2')).toBe('0\n1\n3')
-    })
-  
+  it('should add one number', () => {
+    expect(debug('1')).toBe(`set out 0
+get in.0 1
+set out 1`)
+  })
+
+  it('should add two numbers', () => {
+    expect(debug('1,2')).toBe(`set out 0
+get in.0 1
+set out 1
+get in.1 2
+set out 3`)
   })
 
 })
