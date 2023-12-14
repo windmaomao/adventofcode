ba06eba9884c486580f5fbd78d99858510ba3049
Fang Jin <fang.jin@pearson.com>
2023-12-14 15:04:29 -0500

    2023 day06b

--- 2023/06.js
+++ 2023/06.js
@@ -3,15 +3,8 @@ const read = require("./read");
 const run = require("./run");
 
 const strs = read("06", "\n");
-const parseStrs = (strs) => {
-  const mapped = strs.map((str) => str.match(/\d+/g).map(Number));
-  let res = [];
-  for (let i = 0; i < mapped[0].length; i++) {
-    res.push([mapped[0][i], mapped[1][i]]);
-  }
-  return res;
-};
-const pairs = parseStrs(strs);
+const parseStrs = (strs) => strs.map((str) => str.match(/\d+/g).map(Number));
+const parsed = parseStrs(strs);
 
 const calcRecord = (time, dist) => {
   let m = Math.floor(time / 2);
@@ -22,7 +15,7 @@ const calcRecord = (time, dist) => {
 
   while (res > dist) {
     count++;
-    console.log(m, n, res, count);
+    // console.log(m, n, res, count);
     res -= n;
     n++;
     m--;
@@ -32,11 +25,21 @@ const calcRecord = (time, dist) => {
   return !unique ? count * 2 : count * 2 - 1;
 };
 
-const part1 = (pairs) => {
-  return pairs.map(([time, dist]) => calcRecord(time, dist)).multiply();
+const part1 = (parsed) => {
+  let res = [];
+  for (let i = 0; i < parsed[0].length; i++) {
+    res.push([parsed[0][i], parsed[1][i]]);
+  }
+
+  return res.map(([time, dist]) => calcRecord(time, dist)).multiply();
 };
 
-run(part1, pairs);
-// console.log(calcRecord(7, 9));
-// console.log(calcRecord(15, 40));
-// console.log(calcRecord(30, 200));
+const part2 = (parsed) => {
+  const pair = parsed
+    .map((nums) => nums.map((n) => `${n}`).join(""))
+    .map(Number);
+  return calcRecord(...pair);
+};
+
+run(part1, parsed);
+run(part2, parsed);
