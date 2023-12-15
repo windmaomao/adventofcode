e3581d6ee39d05a48137bb3d7761c89dc3f9ec3c
Fang Jin <fang.jin@pearson.com>
2023-12-15 09:58:28 -0500

    2023 day09b

--- 2023/09.js
+++ 2023/09.js
@@ -6,7 +6,7 @@ const strs = read("09", "\n");
 const parseNumbers = (strs) => strs.map((str) => str.split(" ").map(Number));
 const parsed = parseNumbers(strs);
 
-const calcDiff = (arr) => {
+const forwardDiff = (arr) => {
   const m = [...arr];
   let i = 0,
     done = false;
@@ -21,11 +21,37 @@ const calcDiff = (arr) => {
   return m;
 };
 
-const part1 = (parsed) => {
-  return parsed
-    .map(calcDiff)
+const part1 = (parsed) =>
+  parsed
+    .map(forwardDiff)
     .map((v) => v.sum())
     .sum();
-};
 
 run(part1, parsed);
+
+const backwardDiff = (arr) => {
+  const m = [...arr];
+  let i = 0,
+    done = false;
+  while (!done && i < m.length - 1) {
+    done = true;
+    for (let j = m.length - 1; j > i; j--) {
+      m[j] = m[j] - m[j - 1];
+      if (m[j] != 0) done = false;
+    }
+    i++;
+  }
+  return m;
+};
+
+const part2 = (parsed) =>
+  parsed
+    .map(backwardDiff)
+    .map((arr) => {
+      const initial = arr.pop();
+      return arr.reverse().reduce((acc, v) => v - acc, initial);
+    })
+    .sum();
+
+// console.log(backwardDiff([10, 13, 16, 21, 30, 45]));
+run(part2, parsed);
