8070fc37a6d47e75d4a2488499561e8d7c8d8b6c
Fang Jin <fang.jin@pearson.com>
2023-12-16 17:25:41 -0500

    2023 day12 add permute algorithm

--- 2023/12s.js
+++ 2023/12s.js
@@ -1,4 +1,7 @@
 require("./array");
+const read = require("./read");
+const run = require("./run");
+const strs = read("12.a", "\n");
 
 const parseLine = (str) => {
   const parts = str.split(" ");
@@ -16,16 +19,45 @@ const parseLine = (str) => {
   };
 };
 
-const str = "???.### 1,1,3";
-const parsed = parseLine(str);
-console.log(parsed.pattern.join(""));
+const matchRecord = (template, records, full = false) => {
+  let processed = [...template];
+  const q = template.indexOf("?");
+  if (q > 0) {
+    processed = processed.slice(0, q);
+  }
+
+  const res = processed
+    .join("")
+    .split(/\.+/g)
+    .filter((c) => c != "")
+    .map((v) => v.length);
+
+  let segs = res.length;
+  for (let i = 0; i < segs; i++) {
+    if (i < segs - 1) {
+      if (res[i] != records[i]) return false;
+    } else {
+      if (res[i] > records[i]) return false;
+    }
+  }
+
+  if (!full) return true;
+
+  if (segs != records.length) return false;
+  return res[segs - 1] == records[segs - 1];
+};
 
 // given current filled template, start to work on ith ?
 // and see if can be matched with next records
 const permute = ({ records, fills, count, pattern }) => {
+  let total = 0;
+
   const solve = (template, index) => {
     if (index == fills.length) {
-      console.log(template.join(""));
+      if (matchRecord(template, records, true)) {
+        console.log(template.join(""));
+        total++;
+      }
       return;
     }
 
@@ -33,11 +65,18 @@ const permute = ({ records, fills, count, pattern }) => {
     ["#", "."].forEach((c) => {
       const newTemplate = [...template];
       newTemplate[i] = c;
-      solve(newTemplate, index + 1);
+      if (matchRecord(newTemplate, records)) {
+        solve(newTemplate, index + 1);
+      }
     });
   };
 
   solve(pattern, 0);
+  return total;
 };
 
-permute(parsed);
+const part1 = (strs) => {
+  return strs.map(parseLine).map((o) => permute(o));
+};
+
+run(part1, strs);
