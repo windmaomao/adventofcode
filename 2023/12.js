05791ecec7c4ec4000736bf837332832d98985ec
Fang Jin <fang.jin@pearson.com>
2023-12-16 12:36:29 -0500

    2023 day12b closer

--- 2023/12.js
+++ 2023/12.js
@@ -2,7 +2,7 @@ require("./array");
 const read = require("./read");
 const run = require("./run");
 const permute = require("./permute");
-const strs = read("12", "\n");
+const strs = read("12.c", "\n");
 
 const parseLine = (str) => {
   const parts = str.split(" ");
@@ -32,7 +32,7 @@ const matchPattern = (template, perm, records) => {
     .join("")
     .split(/\.+/g)
     .filter((c) => c != "");
-  // console.log(template.join(""), res);
+  //console.log(template.join(""), res);
 
   const match = res.map((v) => v.length);
   if (match.length != records.length) return false;
@@ -46,13 +46,57 @@ const matchPattern = (template, perm, records) => {
 const part1 = (strs) => {
   return strs
     .map(parseLine)
-    .map((o) => {
+    .map((o, i) => {
       const c = permute(o.fills, o.count)
         .map((perm) => matchPattern([...o.pattern], perm, o.records))
         .filter((v) => v).length;
+      console.log(i, ")", o.fills.length, o.count, c);
       return c ? c : 1;
     })
     .sum();
 };
 
-run(part1, strs);
+// run(part1, strs);
+
+const parseLine2 = (str) => {
+  const parts = str.split(" ");
+  const _pattern = parts[0].split("");
+  const pattern = [..._pattern, "?", ..._pattern];
+  const filled = pattern.filter((c) => c == "#").length;
+  const _records = parts[1].split(",").map(Number);
+  const records = [..._records, ..._records];
+  const count = records.sum() - filled;
+  const fills = pattern.map((_, i) => i).filter((i) => pattern[i] == "?");
+
+  return {
+    pattern,
+    records,
+    count,
+    fills,
+  };
+};
+
+const part2 = (strs) => {
+  const singles = strs.map(parseLine).map((o, i) => {
+    const c = permute(o.fills, o.count)
+      .map((perm) => matchPattern([...o.pattern], perm, o.records))
+      .filter((v) => v).length;
+    return c ? c : 1;
+  });
+
+  const doubles = strs.map(parseLine2).map((o, i) => {
+    const c = permute(o.fills, o.count)
+      .map((perm) => matchPattern([...o.pattern], perm, o.records))
+      .filter((v) => v).length;
+    return c ? c : 1;
+  });
+
+  return singles
+    .map((s, i) => {
+      const m = doubles[i] / s;
+      return s * m * m * m * m;
+    })
+    .sum();
+};
+
+run(part2, strs);
