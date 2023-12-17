b68f8664244b75230af658ebeb6f6467cba7cfe7
Fang Jin <fang.jin@pearson.com>
2023-12-17 18:38:41 -0500

    2023 day15b

--- 2023/15.js
+++ 2023/15.js
@@ -19,3 +19,42 @@ const part1 = (seqs) => {
 };
 
 run(part1, sequences);
+
+const part2 = (seqs) => {
+  const boxes = {};
+
+  seqs.forEach((seq) => {
+    const remove = seq.at(-1) == "-";
+    let label, focal, id;
+    if (remove) {
+      label = seq.slice(0, -1);
+    } else {
+      [label, focal] = seq.split("=");
+    }
+
+    id = hashed(label, 0);
+    boxes[id] = boxes[id] || [];
+
+    if (remove) {
+      boxes[id] = boxes[id].filter((v) => v[0] != label);
+    } else {
+      const found = boxes[id].find((v) => v[0] == label);
+      if (!found) {
+        boxes[id].push([label, focal]);
+      } else {
+        found[1] = focal;
+      }
+    }
+  });
+
+  return Object.keys(boxes)
+    .map((id) => {
+      const box = boxes[id];
+      console.log(box);
+      const k = Number(id) + 1;
+      return box.map((v, i) => k * (i + 1) * v[1]).sum();
+    })
+    .sum();
+};
+
+run(part2, sequences);
