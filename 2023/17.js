538b79c785f1f1ff4187e4b691d7746b40fcdc79
Fang Jin <fang.jin@pearson.com>
2023-12-18 14:24:45 -0500

    2023 day17b closer

--- 2023/17.js
+++ 2023/17.js
@@ -2,7 +2,7 @@ require("./array");
 const read = require("./read");
 const run = require("./run");
 const heapq = require("./heapq");
-const strs = read("17", "\n");
+const strs = read("17.a", "\n");
 const lossMap = strs.map((str) => str.split("").map(Number));
 
 const dirs = [
@@ -64,3 +64,68 @@ function findPath(mat) {
 const part1 = (mat) => findPath(mat);
 
 run(part1, lossMap);
+
+function findPath2(mat) {
+  const _k = (i, j, di, len) => [i, j, di, len].join(":");
+  const m = strs.length;
+  const n = strs[0].length;
+
+  const mark = {};
+  let loss, ux, uy, udi, ulen;
+  let k = 0;
+
+  const heap = [[0, 0, 0, 0, 0]];
+
+  while (heap.length && k < 11115000) {
+    k++;
+    [loss, ux, uy, udi, ulen] = heapq.pop(heap);
+    console.log(loss, ")", ux, uy, udi, ulen);
+
+    // reach the destination
+    if (ux == m - 1 && uy == n - 1) {
+      if (ulen == 4) break;
+    }
+
+    // // visit the current node
+    // const u = _k(ux, uy, udi, ulen);
+    // if (u in mark) continue;
+
+    // mark[u] = true;
+
+    // try all possible directions
+    dirs.forEach(([dx, dy], di) => {
+      // same direction four time
+      const intact = di == udi;
+      if (ulen > 0) {
+        if (ulen < 4) {
+          if (!intact) return;
+        }
+      }
+
+      // can't go back
+      if (di == reverseDirs[udi]) return;
+
+      // inside the grid only
+      const vx = ux + dx;
+      const vy = uy + dy;
+      if (vx < 0 || vx > m - 1) return;
+      if (vy < 0 || vy > n - 1) return;
+
+      console.log("p", loss + mat[vx][vy], ")", vx, vy);
+      // move to next step
+      heapq.push(heap, [
+        loss + mat[vx][vy],
+        vx,
+        vy,
+        di,
+        ulen == 4 ? 1 : ulen + 1,
+      ]);
+    });
+  }
+  console.log("k", k);
+  return loss;
+}
+
+const part2 = (mat) => findPath2(mat);
+
+run(part2, lossMap);
