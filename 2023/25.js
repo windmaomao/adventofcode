5c3745ae20a0aec35a94e5652572b8d66a902bf9
Fang Jin <fang.jin@pearson.com>
2023-12-25 10:24:57 -0500

    2023 day25 setup karger input

--- 2023/25.js
+++ 2023/25.js
@@ -1,28 +1,45 @@
-require("./array");
+require("./object");
 const read = require("./read");
 const run = require("./run");
+const ka = require("./karger");
 const strs = read("25.a", "\n");
 
 const parseGraph = (strs) => {
-  let edges = {};
+  let nodes = [];
+  let edges = [];
+  let indexes = {};
+
+  const add = (u) => {
+    if (!(u in indexes)) {
+      indexes[u] = nodes.length;
+      nodes.push(u);
+    }
+    return indexes[u];
+  };
+
   strs.forEach((str) => {
     const [left, right] = str.split(": ");
-    edges[left] = edges[left] || [];
-    right.split(" ").forEach((v) => {
-      edges[v] = edges[v] || [];
-      edges[left].push(v);
-      edges[v].push(left);
+    const u = add(left);
+
+    right.split(" ").forEach((c) => {
+      const v = add(c);
+      edges.push([u, v]);
     });
   });
-  return {
-    edges,
-  };
+  return { nodes, edges };
 };
 
 const graph = parseGraph(strs);
 
-const part1 = (graph) => {
-  return graph;
+const part1 = ({ nodes, edges }) => {
+  const V = nodes.length;
+  const E = edges.length;
+  let g = new ka.Graph(V, E);
+  // g.edge = edges.map(([u, v]) => new ka.Edge(u, v));
+
+  // add edge 0-1
+  // graph.edge[0] = new Edge(0, 1);
+  return g;
 };
 
 run(part1, graph);
