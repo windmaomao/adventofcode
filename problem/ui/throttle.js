e3593351b8a7fa630f117948640b4e39802935ca
Fang Jin <windmaomao@gmail.com>
2024-05-16 07:13:23 -0400

    update throttle example

--- problem/ui/throttle.js
+++ problem/ui/throttle.js
@@ -1,40 +1,46 @@
-function throttle(fn, delay) {
-	let handle = null
-	let prevArgs = undefined
-		
-	const res = function () {
-		const fire = () => {
-			fn.call(this, ...prevArgs)
-			prevArgs = null
-			handle = setInterval(() => {
-				if (!prevArgs) {
-					handle = clearInterval(handle)
-				} else {
-					fn.call(this, ...prevArgs)
-					prevArgs = null
-				}
-			}, delay)
-		}
-		
-		prevArgs = arguments
-		if (!handle) fire()
-	}
-	
-	res.cancel = function() {
-		prevArgs = null
-	}
-	
-	return res
+// 5/15/24 with reading from solution 2
+function throttle(callback, delay) {
+  let prevArgs = null;
+  let handle = null;
+  let that;
+
+  const fire = () => {
+    callback.call(that, ...prevArgs);
+    prevArgs = null;
+    handle = setInterval(() => {
+      if (prevArgs) {
+        callback.call(that, ...prevArgs);
+        prevArgs = null;
+      } else {
+        handle = clearInterval(handle);
+      }
+    }, delay);
+  };
+
+  function throttled() {
+    that = this;
+    prevArgs = arguments;
+    if (!handle) fire();
+  }
+
+  throttled.cancel = () => {
+    prevArgs = null;
+  };
+
+  return throttled;
 }
+
+// Do not edit the line below.
+exports.throttle = throttle;
 
 const fn = (x) => {
-	console.log(x)
-}
+  console.log(x);
+};
 
-console.log('start ...')
-const d = throttle(fn, 2000)
-d(1)
-d(1)
-d(3)
-d(1)
-d(5)
+console.log("start ...");
+const d = throttle(fn, 2000);
+d(1);
+d(1);
+d(3);
+d(1);
+d(5);
