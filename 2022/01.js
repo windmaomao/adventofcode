903d878448e1127b2c8b2b528cacb7414871feb5
Fang Jin <windmaomao@gmail.com>
2024-11-28 09:41:40 -0500

    refine part2 of day 1

--- 2022/01.js
+++ 2022/01.js
@@ -2,20 +2,15 @@ require("./array");
 const read = require("./read");
 const run = require("./run");
 
-const part1 = (strs) => {
-  return strs
-    .split("")
-    .map((arr) => arr.sum((v) => Number(v)))
-    .max();
-};
+const part1 = (items) => items.max();
 
-const part2 = (strs) =>
-  findAll(strs)
+const part2 = (items) =>
+  items
     .sort((a, b) => b - a)
     .take(3)
     .sum();
 
 const strs = read("01", "\n");
-strs.push("");
-run(part1, strs);
-// run(part2, strs);
+const items = strs.split("").map((arr) => arr.sum((v) => Number(v)));
+run(part1, items);
+run(part2, items);
