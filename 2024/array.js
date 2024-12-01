f9c27b7aaecc9485c0603a02386cce1a35de8ee7
Fang Jin <windmaomao@gmail.com>
2024-12-01 08:52:13 -0500

    add 2024 day 1

---
+++ 2024/array.js
@@ -0,0 +1,76 @@
+// [].new(n)
+Array.prototype.new = function (n) {
+  return new Array(n).fill(0);
+};
+
+// a.max()
+Array.prototype.max = function (n) {
+  return Math.max(...this);
+};
+
+// a.min()
+Array.prototype.min = function (n) {
+  return Math.min(...this);
+};
+
+// a.take(3)
+Array.prototype.take = function (n) {
+  return this.slice(0, n);
+};
+
+// a.sum()
+// a.sum((v, i) => v + fn(i))
+Array.prototype.sum = function (fn) {
+  if (!fn) {
+    return this.reduce((acc, v) => acc + v, 0);
+  } else {
+    return this.reduce(
+      (acc, v, i, arr) => acc + fn(v, i, arr),
+      0
+    );
+  }
+};
+
+// a.multiply()
+// a.multiply((v, i) => v + fn(i))
+Array.prototype.multiply = function (fn) {
+  if (!fn) {
+    return this.reduce((acc, v) => acc * v, 1);
+  } else {
+    return this.reduce(
+      (acc, v, i, arr) => acc * fn(v, i, arr),
+      1
+    );
+  }
+};
+
+Array.prototype.split = function (
+  separator = ""
+) {
+  let arr = this;
+  let res = [],
+    tmp = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (separator == arr[i]) {
+      res.push(tmp);
+      tmp = [];
+    } else {
+      tmp.push(arr[i]);
+    }
+  }
+  if (tmp.length) res.push(tmp);
+  return res;
+};
+
+// https://stackoverflow.com/questions/8495687/split-array-into-chunks
+Array.prototype.chunk = function (chunkSize) {
+  var array = this;
+  return [].concat.apply(
+    [],
+    array.map(function (elem, i) {
+      return i % chunkSize
+        ? []
+        : [array.slice(i, i + chunkSize)];
+    })
+  );
+};
