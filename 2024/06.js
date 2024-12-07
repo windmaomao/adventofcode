f8df39908847bd9289b497d776f27e1fbcc75f0b
Fang Jin <windmaomao@gmail.com>
2024-12-06 21:45:15 -0500

    add 2024 day 6 part2 not working

--- 2024/06.js
+++ 2024/06.js
@@ -53,4 +53,59 @@ const part1 = (strs) => {
   return Object.keys(visited).length
 }
 
-run(part1, strs)
+// run(part1, strs)
+
+const part2 = (strs) => {
+  let pos = [...origin]
+  let d = 0,
+    x,
+    y
+  const visited = {},
+    obstructs = {}
+  let finished = false
+
+  while (!finished) {
+    const dir = dirs[d]
+    x = pos[0] + dir[0]
+    y = pos[1] + dir[1]
+
+    if (!strs[x] || !strs[x][y]) {
+      finished = true
+      continue
+    }
+
+    if (strs[x][y] === "#") {
+      d++
+      if (d === 4) d = 0
+      continue
+    }
+
+    nd = d + 1
+    if (nd === 4) nd = 0
+    let done = false
+    let nx = pos[0],
+      ny = pos[1]
+    while (!done) {
+      nx += dirs[nd][0]
+      ny += dirs[nd][1]
+      if (
+        !strs[nx] ||
+        !strs[nx][ny] ||
+        strs[nx][ny] == "#"
+      ) {
+        done = true
+        continue
+      }
+      if (visited[`${nx},${ny}`] === nd) {
+        obstructs[`${x},${y}`] = true
+      }
+    }
+
+    visited[`${x},${y}`] = d
+    pos = [x, y]
+  }
+
+  return Object.keys(obstructs).length
+}
+
+run(part2, strs)
