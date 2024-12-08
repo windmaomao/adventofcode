659cac8169deb57685c79456d90b7ac23c27aaa5
Fang Jin <windmaomao@gmail.com>
2024-12-07 21:19:04 -0500

    add 2024 day 7 part2

--- 2024/07.js
+++ 2024/07.js
@@ -37,3 +37,35 @@ const part1 = (rows) =>
     .sum()
 
 run(part1, rows)
+
+function findSum2(sum, nums) {
+  const n = nums.length
+  function isGoal(start, i, op) {
+    if (start > sum) return false
+    if (i === n) {
+      if (start === sum) return true
+      return false
+    }
+
+    let next = start
+    if (op == 0) next += nums[i]
+    if (op == 1) next *= nums[i]
+    if (op == 2) next = Number(`${next}${nums[i]}`)
+
+    return (
+      isGoal(next, i + 1, 0) ||
+      isGoal(next, i + 1, 1) ||
+      isGoal(next, i + 1, 2)
+    )
+  }
+
+  return isGoal(0, 0, 0)
+}
+
+const part2 = (rows) =>
+  rows
+    .filter(({ sum, nums }) => findSum2(sum, nums))
+    .map((r) => r.sum)
+    .sum()
+
+run(part2, rows)
