811f4e2f201555743b485c069c705d610fae7cbe
Fang Jin <windmaomao@gmail.com>
2024-12-11 16:24:13 -0500

    add 2024 day 11 part2

--- 2024/11.js
+++ 2024/11.js
@@ -6,23 +6,34 @@ const strs = read("11", "\n")
 const nums = strs[0].split(" ").map(Number)
 
 function blinks(nums, total) {
+  const visited = {}
+
   function getTotal(i, count) {
     if (count == 0) return 1
 
-    if (i == 0) return getTotal(1, count - 1)
-    let digits = `${i}`
-    let dn = digits.length
-    if (dn % 2 == 0) {
-      let half = dn / 2
-      const left = Number(digits.slice(0, half))
-      const right = Number(digits.slice(half))
-      return (
-        getTotal(left, count - 1) +
-        getTotal(right, count - 1)
-      )
+    const k = `${i},${count}`
+    if (visited[k]) return visited[k]
+
+    let res
+    if (i == 0) {
+      res = getTotal(1, count - 1)
     } else {
-      return getTotal(i * 2024, count - 1)
+      let digits = `${i}`
+      let dn = digits.length
+      if (dn % 2 == 0) {
+        let half = dn / 2
+        const left = Number(digits.slice(0, half))
+        const right = Number(digits.slice(half))
+        res =
+          getTotal(left, count - 1) +
+          getTotal(right, count - 1)
+      } else {
+        res = getTotal(i * 2024, count - 1)
+      }
     }
+
+    visited[k] = res
+    return res
   }
 
   return nums.map((i) => getTotal(i, total)).sum()
