eb2edac3f3003819586e59a51d186e2270a16f63
Fang Jin <windmaomao@gmail.com>
2024-12-12 21:14:22 -0500

    add 2024 day 12 part2 not working

--- 2024/12.js
+++ 2024/12.js
@@ -2,7 +2,7 @@ require("./array")
 const read = require("./read")
 const run = require("./run")
 
-const map = read("12", "\n")
+const map = read("12.b", "\n")
 const m = map.length
 const n = map[0].length
 
@@ -81,3 +81,78 @@ const part1 = (map) => {
 }
 
 run(part1, map)
+
+const rows = (arr) => {
+  const sides = {}
+
+  for (let [i, j] of arr) {
+    ;[
+      [i, j],
+      [i + 1, j],
+    ].forEach(([p, q]) => {
+      sides[p] = sides[p] || {}
+      sides[p][q] = sides[p][q] ? false : true
+    })
+  }
+
+  return Object.values(sides)
+    .map((o) => {
+      const list = Object.keys(o)
+        .filter((k) => o[k])
+        .map(Number)
+
+      let c = 0,
+        prev = -2
+      for (let i = 0; i < list.length; i++) {
+        if (list[i] - 1 != prev) {
+          c++
+        }
+        prev = list[i]
+      }
+
+      return c
+    })
+    .sum()
+}
+
+const cols = (arr) => {
+  const sides = {}
+
+  for (let [i, j] of arr) {
+    ;[
+      [i, j],
+      [i, j + 1],
+    ].forEach(([p, q]) => {
+      sides[q] = sides[q] || {}
+      sides[q][p] = sides[q][p] ? false : true
+    })
+  }
+
+  return Object.values(sides)
+    .map((o) => {
+      const list = Object.keys(o)
+        .filter((k) => o[k])
+        .map(Number)
+
+      let c = 0,
+        prev = -2
+      for (let i = 0; i < list.length; i++) {
+        if (list[i] - 1 != prev) {
+          c++
+        }
+        prev = list[i]
+      }
+
+      return c
+    })
+    .sum()
+}
+
+const part2 = (map) => {
+  const areas = findAreas(map)
+  return areas
+    .map((arr) => (rows(arr) + cols(arr)) * area(arr))
+    .sum()
+}
+
+run(part2, map)
